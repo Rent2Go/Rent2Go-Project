0201c4582f3d8aa7e38852b1a45d27cdb547bf13
Soner <syln.soner@gmail.com>
2023-12-08 23:38:09 +0300

    Rent2Go project created

---
+++ src/main/java/com/example/rent2gojavaproject/Rent2GoJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rent2gojavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rent2GoJavaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rent2GoJavaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rent2gojavaproject/Rent2GoJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rent2gojavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rent2GoJavaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
