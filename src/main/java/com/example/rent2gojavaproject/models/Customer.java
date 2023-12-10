bf303312335a0adc532e28509c818d8838202e0c
Yagmur Curku <yagmurcurku@gmail.com>
2023-12-10 20:57:42 +0300

    created Customer class

--- src/main/java/com/example/rent2gojavaproject/models/Customer.java
+++ src/main/java/com/example/rent2gojavaproject/models/Customer.java
@@ -1,4 +1,27 @@
 package com.example.rent2gojavaproject.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "nationality_id")
+    private String nationalityId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
--- src/main/java/com/example/rent2gojavaproject/models/User.java
+++ src/main/java/com/example/rent2gojavaproject/models/User.java
@@ -1,9 +1,12 @@
 package com.example.rent2gojavaproject.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -28,5 +31,9 @@ public class User {
 
     @Column(name = "email", unique = true, nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Customer> customers;
 
 }
