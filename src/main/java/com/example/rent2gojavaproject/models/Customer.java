c0bbeb740ad3beeca94d67c26e960880f9475d5c
Yagmur Curku <yagmurcurku@gmail.com>
2023-12-10 20:51:39 +0300

    created User class

---
+++ src/main/java/com/example/rent2gojavaproject/models/Customer.java
@@ -0,0 +1,4 @@
+package com.example.rent2gojavaproject.models;
+
+public class Customer {
+}
---
+++ src/main/java/com/example/rent2gojavaproject/models/User.java
@@ -0,0 +1,32 @@
+package com.example.rent2gojavaproject.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "gsm", unique = true, nullable = false)
+    private String gsm;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+}
