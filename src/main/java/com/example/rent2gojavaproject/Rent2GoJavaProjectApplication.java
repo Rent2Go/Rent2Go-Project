e17baa9473551be0105e93b92908ef6a4d633cdd
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-10 16:16:35 +0300

    test commit

--- src/main/java/com/example/rent2gojavaproject/Rent2GoJavaProjectApplication.java
+++ src/main/java/com/example/rent2gojavaproject/Rent2GoJavaProjectApplication.java
@@ -8,6 +8,8 @@ public class Rent2GoJavaProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Rent2GoJavaProjectApplication.class, args);
+
+
     }
 
 }
