d5745e59cb5f42e04bca4b5aca17051b57aea6f7
ShmsErl <erol.sehmus1@gmail.com>
2023-12-11 14:07:09 +0300

    BrandsController class created.

--- src/main/java/com/example/rent2gojavaproject/controllers/BrandsController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/BrandsController.java
@@ -1,4 +1,9 @@
 package com.example.rent2gojavaproject.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/V1/brands")
 public class BrandsController {
 }
