18f1cc9c841e2d1b90d9a76cbcce98475f47208b
ShmsErl <erol.sehmus1@gmail.com>
2023-12-11 14:10:13 +0300

    CustomerController class created.

---
+++ src/main/java/com/example/rent2gojavaproject/controllers/CustomersController.java
@@ -0,0 +1,9 @@
+package com.example.rent2gojavaproject.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/V1/customers")
+public class CustomersController {
+}
