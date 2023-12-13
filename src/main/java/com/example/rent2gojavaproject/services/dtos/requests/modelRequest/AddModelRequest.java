8605cd4e2a833c19c13b73f0abe18ba87757ba2e
ShmsErl <erol.sehmus1@gmail.com>
2023-12-13 14:37:59 +0300

    AddBrandRequest dto class fiiled and updated.

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/modelRequest/AddModelRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/modelRequest/AddModelRequest.java
@@ -1,4 +1,15 @@
 package com.example.rent2gojavaproject.services.dtos.requests.modelRequest;
 
+import com.example.rent2gojavaproject.services.dtos.requests.brandRequest.AddBrandRequest;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddModelRequest {
+
+    private String name;
+    private int brandId;
 }
