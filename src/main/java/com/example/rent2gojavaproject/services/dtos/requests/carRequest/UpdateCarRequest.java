1a153ef1dd5fe503b77733e067994d89817d0440
Soner <syln.soner@gmail.com>
2023-12-13 18:18:32 +0300

    added validation rules update and add colorrequests

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/carRequest/UpdateCarRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/carRequest/UpdateCarRequest.java
@@ -1,9 +1,6 @@
 package com.example.rent2gojavaproject.services.dtos.requests.carRequest;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,12 +19,17 @@ public class UpdateCarRequest {
     @Min(value = 2005, message = "Year must be greater than or equal to 2005.")
     @Max(value = 2024, message = "Year must be less than or equal to the current year.")
     private int year;
-
+    @NotNull(message = "The daily price field cannot be null.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Daily price must be greater than 0.")
     private double dailyPrice;
-
+    @NotBlank(message = "The plate field can't be empty.")
+    @Pattern(regexp = "(0[1-9]|[1-7][0-9]|8[01])[A-Z]{1,3}(\\d{2}|\\d{4})", message = "Invalid licence plate")
+    @Size(min = 5, max = 9, message = "Licence plate must be between 5 and 9 characters")
     private String plate;
-
+    @NotNull(message = "The model id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int modelId;
-
+    @NotNull(message = "The color id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int colorId;
 }
