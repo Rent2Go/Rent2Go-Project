37d201d64a2aab91ab4d7acd57a9b552362b3a52
Soner <syln.soner@gmail.com>
2023-12-13 16:49:45 +0300

    added AddBrandRequest and UpdateBrandRequest validation rules

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/brandRequest/AddBrandRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/brandRequest/AddBrandRequest.java
@@ -1,5 +1,9 @@
 package com.example.rent2gojavaproject.services.dtos.requests.brandRequest;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +12,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddBrandRequest {
+    @NotNull(message = "The brand name cannot be null.")
+    @NotBlank(message = "The brand name can't be empty.")
+    @Size(min = 3, max = 15, message = "Brand name should be between 3 and 15 digits.")
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "The brand name should only contain letters.")
     private String name;
 }
--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/brandRequest/UpdateBrandRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/brandRequest/UpdateBrandRequest.java
@@ -1,5 +1,6 @@
 package com.example.rent2gojavaproject.services.dtos.requests.brandRequest;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateBrandRequest {
+    @NotNull(message = "The brand id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int id;
+    @NotNull(message = "The brand name cannot be null.")
+    @NotBlank(message = "The brand name can't be empty.")
+    @Size(min = 3, max = 15, message = "Brand name should be between 3 and 15 digits.")
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "The brand name should only contain letters.")
     private String name;
 }
