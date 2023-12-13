798cc4eea8f6646a90dc894c2e0b925693abae81
Soner <syln.soner@gmail.com>
2023-12-13 15:42:35 +0300

    AddColorRequest and UpdateColorRequest updated

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/AddColorRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/AddColorRequest.java
@@ -1,4 +1,12 @@
 package com.example.rent2gojavaproject.services.dtos.requests.colorRequest;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddColorRequest {
+    private String name;
 }
--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/UpdateColorRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/UpdateColorRequest.java
@@ -1,4 +1,13 @@
 package com.example.rent2gojavaproject.services.dtos.requests.colorRequest;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateColorRequest {
+    private int id;
+    private String name;
 }
