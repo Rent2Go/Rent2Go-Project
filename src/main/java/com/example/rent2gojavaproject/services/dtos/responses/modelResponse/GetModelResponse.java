118c939b086f042579d30260faab74976230028d
ShmsErl <erol.sehmus1@gmail.com>
2023-12-13 15:10:28 +0300

    'id' property added to GetModelResponse dto class.

--- src/main/java/com/example/rent2gojavaproject/services/dtos/responses/modelResponse/GetModelResponse.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/modelResponse/GetModelResponse.java
@@ -1,11 +1,17 @@
 package com.example.rent2gojavaproject.services.dtos.responses.modelResponse;
 
 import com.example.rent2gojavaproject.services.dtos.responses.brandResponse.GetBrandResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetModelResponse {
+    private int id;
     private String name;
-
-    private GetBrandResponse brand;
+    private GetBrandResponse brandResponse;
 
 
 }
