ced306d1d06dcb9f55ccfcf5d532b884acea4c4e
ShmsErl <erol.sehmus1@gmail.com>
2023-12-13 13:59:37 +0300

    GetCarResponse dto class fiiled and updated.

--- src/main/java/com/example/rent2gojavaproject/services/dtos/responses/carResponse/GetCarResponse.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/carResponse/GetCarResponse.java
@@ -1,4 +1,16 @@
 package com.example.rent2gojavaproject.services.dtos.responses.carResponse;
 
+import com.example.rent2gojavaproject.services.dtos.responses.modelResponse.GetModelResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetCarResponse {
+
+    private String name;
+    private GetModelResponse model;
+    private GetCarResponse color;
 }
