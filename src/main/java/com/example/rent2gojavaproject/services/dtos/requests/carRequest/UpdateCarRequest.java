0d36616bf358e6a93ccffb2df75ef05ec7aa07b3
Soner <syln.soner@gmail.com>
2023-12-13 18:00:13 +0300

    UpdateCarRequest updated

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/carRequest/UpdateCarRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/carRequest/UpdateCarRequest.java
@@ -1,5 +1,9 @@
 package com.example.rent2gojavaproject.services.dtos.requests.carRequest;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +12,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateCarRequest {
+    @NotNull(message = "The car id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int id;
+    @NotNull(message = "The kilometer field cannot be null.")
+    @Min(value = 0, message = "Kilometer must be greater than or equal to 0.")
     private int kilometer;
-
+    @NotNull(message = "The year field cannot be null.")
+    @Min(value = 2005, message = "Year must be greater than or equal to 2005.")
+    @Max(value = 2024, message = "Year must be less than or equal to the current year.")
     private int year;
 
     private double dailyPrice;
