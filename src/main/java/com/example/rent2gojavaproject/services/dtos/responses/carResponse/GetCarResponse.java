9c086b0efdba7087b76328d2d9854825f7343389
ShmsErl <erol.sehmus1@gmail.com>
2023-12-13 15:34:16 +0300

    GetCarListResponse and GetCarResponse dto classes filled and updated.

--- src/main/java/com/example/rent2gojavaproject/services/dtos/responses/carResponse/GetCarResponse.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/carResponse/GetCarResponse.java
@@ -1,5 +1,6 @@
 package com.example.rent2gojavaproject.services.dtos.responses.carResponse;
 
+import com.example.rent2gojavaproject.services.dtos.responses.colorResponse.GetColorResponse;
 import com.example.rent2gojavaproject.services.dtos.responses.modelResponse.GetModelResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,8 +10,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetCarResponse {
+    private int id;
 
-    private String name;
-    private GetModelResponse model;
-    private GetCarResponse color;
+    private int kilometer;
+
+    private int year;
+
+    private double dailyPrice;
+
+    private String plate;
+
+    private GetModelResponse modelResponse;
+
+    private GetColorResponse colorResponse ;
+
 }
