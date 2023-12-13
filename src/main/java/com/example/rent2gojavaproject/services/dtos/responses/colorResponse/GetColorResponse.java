8d24e0291970348cbf7df4e6083d6cbdc8390ce5
ShmsErl <erol.sehmus1@gmail.com>
2023-12-13 13:50:54 +0300

    GetColorResponse dto class fiiled and updated.

--- src/main/java/com/example/rent2gojavaproject/services/dtos/responses/colorResponse/GetColorResponse.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/colorResponse/GetColorResponse.java
@@ -1,4 +1,14 @@
 package com.example.rent2gojavaproject.services.dtos.responses.colorResponse;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetColorResponse {
+
+
+    private String name;
 }
