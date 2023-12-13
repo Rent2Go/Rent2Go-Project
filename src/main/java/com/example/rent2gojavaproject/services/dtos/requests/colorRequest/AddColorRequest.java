db5ba3de7bf6261c1b8d00692d93530276093738
Soner <syln.soner@gmail.com>
2023-12-13 16:54:05 +0300

    validation rules added

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/AddColorRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/AddColorRequest.java
@@ -1,5 +1,9 @@
 package com.example.rent2gojavaproject.services.dtos.requests.colorRequest;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +12,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddColorRequest {
+    @NotNull(message = "The color name cannot be null.")
+    @NotBlank(message = "The color name can't be empty.")
+    @Size(min = 3, max = 15, message = "Color name should be between 3 and 15 digits.")
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "The color name should only contain letters.")
     private String name;
 }
--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/UpdateColorRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/UpdateColorRequest.java
@@ -1,5 +1,6 @@
 package com.example.rent2gojavaproject.services.dtos.requests.colorRequest;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateColorRequest {
+    @NotNull(message = "The color id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int id;
+    @NotNull(message = "The color name cannot be null.")
+    @NotBlank(message = "The color name can't be empty.")
+    @Size(min = 3, max = 15, message = "Color name should be between 3 and 15 digits.")
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "The color name should only contain letters.")
     private String name;
 }
