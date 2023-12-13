73ebab4a3d30031a9eda279e5ba6df4a37f6d4ba
Soner <syln.soner@gmail.com>
2023-12-13 15:44:11 +0300

    AddModelRequest and UpdateModelRequest filled-updated

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/modelRequest/UpdateModelRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/modelRequest/UpdateModelRequest.java
@@ -1,4 +1,14 @@
 package com.example.rent2gojavaproject.services.dtos.requests.modelRequest;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateModelRequest {
+    private int id;
+    private String name;
+    private int brandId;
 }
