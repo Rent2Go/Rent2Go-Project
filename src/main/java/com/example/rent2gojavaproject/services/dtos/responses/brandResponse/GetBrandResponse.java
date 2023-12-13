bd9ab8d9ba67a8e1e13ebd390797d7be70839780
ShmsErl <erol.sehmus1@gmail.com>
2023-12-13 13:55:22 +0300

    GetBrandResponse dto class fiiled and updated.

--- src/main/java/com/example/rent2gojavaproject/services/dtos/responses/brandResponse/GetBrandResponse.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/brandResponse/GetBrandResponse.java
@@ -1,4 +1,12 @@
 package com.example.rent2gojavaproject.services.dtos.responses.brandResponse;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetBrandResponse {
+    private String name;
 }
