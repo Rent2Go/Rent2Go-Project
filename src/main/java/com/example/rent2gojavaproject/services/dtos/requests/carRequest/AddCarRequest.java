ef2278560a028ca3fe5ffaa8a91805c57acb210e
ShmsErl <erol.sehmus1@gmail.com>
2023-12-13 14:43:38 +0300

    AddCarRequest dto class fiiled and updated.

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/carRequest/AddCarRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/carRequest/AddCarRequest.java
@@ -1,4 +1,24 @@
 package com.example.rent2gojavaproject.services.dtos.requests.carRequest;
 
+import com.example.rent2gojavaproject.models.Color;
+import com.example.rent2gojavaproject.models.Model;
+import jakarta.persistence.*;
+
 public class AddCarRequest {
+
+
+///Get by model response ve colorresponse oluşturulacak.
+
+    private int kilometer;
+
+    private int year;
+
+    private double dailyPrice;
+
+    private String plate;
+
+    private int modelId;
+
+    private int colorId;
+
 }
