4624518cff278541b7dbc430ab80cea5b7346890
Soner <syln.soner@gmail.com>
2023-12-16 00:38:27 +0300

    added entities adduserrequest

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/AddUserRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/AddUserRequest.java
@@ -1,4 +1,11 @@
 package com.example.rent2gojavaproject.services.dtos.requests.userRequest;
 
 public class AddUserRequest {
+    private String name;
+
+    private String surname;
+
+    private String phoneNumber;
+
+    private String email;
 }
