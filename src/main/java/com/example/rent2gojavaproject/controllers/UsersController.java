e7c4cbbbbb39980b293ed38c76020c45e0b6be3a
Soner <syln.soner@gmail.com>
2023-12-16 00:59:47 +0300

    added PostMapping

--- src/main/java/com/example/rent2gojavaproject/controllers/UsersController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/UsersController.java
@@ -1,14 +1,15 @@
 package com.example.rent2gojavaproject.controllers;
 
 import com.example.rent2gojavaproject.core.utilities.results.DataResult;
+import com.example.rent2gojavaproject.core.utilities.results.Result;
 import com.example.rent2gojavaproject.services.abstracts.UserService;
+import com.example.rent2gojavaproject.services.dtos.requests.userRequest.AddUserRequest;
 import com.example.rent2gojavaproject.services.dtos.responses.userResponse.GetUserListResponse;
 import com.example.rent2gojavaproject.services.dtos.responses.userResponse.GetUserResponse;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +27,11 @@ public class UsersController {
     @GetMapping("/{id}")
     public DataResult<GetUserResponse> getUserById(@PathVariable int id) {
         return userService.getById(id);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Result createUser(@RequestBody @Valid AddUserRequest addUserRequest) {
+        return userService.addUser(addUserRequest);
     }
 }
