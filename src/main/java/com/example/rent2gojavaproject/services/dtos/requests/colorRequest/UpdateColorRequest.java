faf14569ea308656bbf3c0a2ef156ea5e0817e1f
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-15 16:08:03 +0300

    updated color size as min 2

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/UpdateColorRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/colorRequest/UpdateColorRequest.java
@@ -14,7 +14,7 @@ public class UpdateColorRequest {
     private int id;
     @NotNull(message = "The color name cannot be null.")
     @NotBlank(message = "The color name can't be empty.")
-    @Size(min = 3, max = 15, message = "Color name should be between 3 and 15 digits.")
+    @Size(min = 2, max = 15, message = "Color name should be between 2 and 15 digits.")
     @Pattern(regexp = "^[a-zA-Z]*$", message = "The color name should only contain letters.")
     private String name;
 }
