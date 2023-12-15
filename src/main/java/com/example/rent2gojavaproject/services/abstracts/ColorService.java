615f78c20364fc80e73b709cc2deb7466a5e3370
Yagmur Curku <yagmurcurku@gmail.com>
2023-12-15 20:16:12 +0300

    created ColorService

--- src/main/java/com/example/rent2gojavaproject/services/abstracts/ColorService.java
+++ src/main/java/com/example/rent2gojavaproject/services/abstracts/ColorService.java
@@ -1,4 +1,22 @@
 package com.example.rent2gojavaproject.services.abstracts;
 
+import com.example.rent2gojavaproject.services.dtos.requests.colorRequest.AddColorRequest;
+import com.example.rent2gojavaproject.services.dtos.requests.colorRequest.UpdateColorRequest;
+import com.example.rent2gojavaproject.services.dtos.responses.colorResponse.GetColorListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.colorResponse.GetColorResponse;
+
+import java.util.List;
+
 public interface ColorService {
+
+    List<GetColorListResponse> getAllColors();
+
+    GetColorResponse getById(int id);
+
+    String addColor(AddColorRequest addColorRequest);
+
+    String updateColor(UpdateColorRequest updateColorRequest);
+
+    String deleteColor(int id);
+
 }
