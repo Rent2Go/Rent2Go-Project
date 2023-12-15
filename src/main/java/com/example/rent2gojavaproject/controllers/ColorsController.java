d3da7f214a067f855de2f059ca1f430eba1669ca
Yagmur Curku <yagmurcurku@gmail.com>
2023-12-16 00:21:32 +0300

    added getById method to ColorsController

--- src/main/java/com/example/rent2gojavaproject/controllers/ColorsController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/ColorsController.java
@@ -2,8 +2,10 @@ package com.example.rent2gojavaproject.controllers;
 
 import com.example.rent2gojavaproject.services.abstracts.ColorService;
 import com.example.rent2gojavaproject.services.dtos.responses.colorResponse.GetColorListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.colorResponse.GetColorResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,12 @@ public class ColorsController {
     List<GetColorListResponse> getAllColor(){
 
         return this.colorService.getAllColors();
+    }
+
+    @GetMapping("/{id}")
+    GetColorResponse getById(@PathVariable int id){
+
+        return this.colorService.getById(id);
     }
 
 
