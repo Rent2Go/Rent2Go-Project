c0332c9e97aeeb4d99383c74ada2ced545a25ff3
ShmsErl <erol.sehmus1@gmail.com>
2023-12-15 22:27:55 +0300

    EmployeeManager class filled and updated.

--- src/main/java/com/example/rent2gojavaproject/services/concretes/EmployeeManager.java
+++ src/main/java/com/example/rent2gojavaproject/services/concretes/EmployeeManager.java
@@ -4,6 +4,7 @@ import com.example.rent2gojavaproject.core.utilities.mappers.ModelMapperService;
 import com.example.rent2gojavaproject.core.utilities.results.DataResult;
 import com.example.rent2gojavaproject.core.utilities.results.Result;
 import com.example.rent2gojavaproject.core.utilities.results.SuccessDataResult;
+import com.example.rent2gojavaproject.core.utilities.results.SuccessResult;
 import com.example.rent2gojavaproject.models.Employee;
 import com.example.rent2gojavaproject.repositories.EmployeeRepository;
 import com.example.rent2gojavaproject.services.abstracts.EmployeeService;
@@ -23,36 +24,47 @@ public class EmployeeManager implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
     private ModelMapperService mapperService;
+
     @Override
     public DataResult<List<GetEmployeeListResponse>> getAllEmployees() {
         List<Employee> employees = this.employeeRepository.findAll();
-        List<GetEmployeeListResponse> responses = employees.stream().map(employee -> this.mapperService
-                .forResponse()
-                .map(employee,GetEmployeeListResponse.class))
-                .collect(Collectors.toList());
+        List<GetEmployeeListResponse> responses = employees.stream().map(employee -> this.mapperService.forResponse().map(employee, GetEmployeeListResponse.class)).collect(Collectors.toList());
 
 
-
-        return new SuccessDataResult<List<GetEmployeeListResponse>>(responses);
+        return new SuccessDataResult<List<GetEmployeeListResponse>>(responses,"Transaction Successfully");
     }
 
     @Override
     public DataResult<GetEmployeeResponse> getById(int id) {
-        return null;
+        Employee employee = this.employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Couldn't find employee id"));
+
+        GetEmployeeResponse response = this.mapperService.forResponse().map(employee, GetEmployeeResponse.class);
+
+
+        return new SuccessDataResult<GetEmployeeResponse>(response, "Transaction Successfully");
     }
 
     @Override
     public Result addEmployee(AddEmployeeRequest addEmployeeRequest) {
-        return null;
+        Employee employee = this.mapperService.forRequest().map(addEmployeeRequest, Employee.class);
+
+        this.employeeRepository.save(employee);
+        return new SuccessResult("Added employee successfully");
     }
 
     @Override
     public Result updateEmployee(UpdateEmployeeRequest updateEmployeeRequest) {
-        return null;
+        Employee employee = this.employeeRepository.findById(updateEmployeeRequest.getId()).orElseThrow(() -> new RuntimeException("Couldn't find employee id"));
+
+        employee = this.mapperService.forRequest().map(updateEmployeeRequest, Employee.class);
+        this.employeeRepository.save(employee);
+        return new SuccessResult("Updated employee successfully");
     }
 
     @Override
     public Result deleteEmployee(int id) {
-        return null;
+        this.employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Couldn't find employee id"));
+        this.employeeRepository.deleteById(id);
+        return new SuccessResult("Deleted employee successfully");
     }
 }
