534c803e4dfdc0a7c44705720c44da37e8d397b4
Yagmur Curku <yagmurcurku@gmail.com>
2023-12-16 00:20:07 +0300

    added getAllColor method to ColorsController

--- src/main/java/com/example/rent2gojavaproject/controllers/ColorsController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/ColorsController.java
@@ -1,9 +1,26 @@
 package com.example.rent2gojavaproject.controllers;
 
+import com.example.rent2gojavaproject.services.abstracts.ColorService;
+import com.example.rent2gojavaproject.services.dtos.responses.colorResponse.GetColorListResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/V1/colors")
+@AllArgsConstructor
 public class ColorsController {
+
+    private final ColorService colorService;
+
+    @GetMapping("/getAll")
+    List<GetColorListResponse> getAllColor(){
+
+        return this.colorService.getAllColors();
+    }
+
+
 }
