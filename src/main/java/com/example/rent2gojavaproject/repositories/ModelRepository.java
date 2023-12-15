14b92f684ac31313fbd798f3199a893e31e8b2e3
Soner <syln.soner@gmail.com>
2023-12-16 00:25:21 +0300

    Filled ModelRepository

--- src/main/java/com/example/rent2gojavaproject/repositories/ModelRepository.java
+++ src/main/java/com/example/rent2gojavaproject/repositories/ModelRepository.java
@@ -1,8 +1,12 @@
 package com.example.rent2gojavaproject.repositories;
 
 import com.example.rent2gojavaproject.models.Model;
+import com.example.rent2gojavaproject.services.dtos.responses.modelResponse.GetModelListResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ModelRepository extends JpaRepository<Model,Integer> {
+    List<GetModelListResponse> getAllModels();
 
 }
