396a284ab8f7eceac97d9d18bbdaf804ead3fbcb
Soner <syln.soner@gmail.com>
2023-12-16 01:45:26 +0300

    updated DiscountList & DiscountResponse

--- src/main/java/com/example/rent2gojavaproject/services/dtos/responses/discountResponse/GetDiscountListResponse.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/discountResponse/GetDiscountListResponse.java
@@ -1,4 +1,13 @@
 package com.example.rent2gojavaproject.services.dtos.responses.discountResponse;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetDiscountListResponse {
+    private int id;
+    private String discountCode;
+    private double percentage;
+    private int rentalId;
 }
