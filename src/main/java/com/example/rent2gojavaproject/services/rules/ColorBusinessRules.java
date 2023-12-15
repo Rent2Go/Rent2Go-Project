6db62ed25903c44a82a3a9af8ceb9be158aaeafe
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-16 00:59:11 +0300

    fixed bug

--- src/main/java/com/example/rent2gojavaproject/services/rules/ColorBusinessRules.java
+++ src/main/java/com/example/rent2gojavaproject/services/rules/ColorBusinessRules.java
@@ -1,6 +1,9 @@
 package com.example.rent2gojavaproject.services.rules;
 
 import com.example.rent2gojavaproject.repositories.ColorRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 
 public class ColorBusinessRules {
 
