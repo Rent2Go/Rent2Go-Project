42154069fec56642cf957fe7ec825a0eee52ae31
Soner <syln.soner@gmail.com>
2023-12-16 00:45:11 +0300

    UserService & Manager Updated

--- src/main/java/com/example/rent2gojavaproject/services/concretes/UserManager.java
+++ src/main/java/com/example/rent2gojavaproject/services/concretes/UserManager.java
@@ -1,4 +1,53 @@
 package com.example.rent2gojavaproject.services.concretes;
 
-public class UserManager {
+import com.example.rent2gojavaproject.core.utilities.mappers.ModelMapperService;
+import com.example.rent2gojavaproject.core.utilities.results.DataResult;
+import com.example.rent2gojavaproject.core.utilities.results.Result;
+import com.example.rent2gojavaproject.core.utilities.results.SuccessDataResult;
+import com.example.rent2gojavaproject.models.User;
+import com.example.rent2gojavaproject.repositories.UserRepository;
+import com.example.rent2gojavaproject.services.abstracts.UserService;
+import com.example.rent2gojavaproject.services.dtos.requests.userRequest.AddUserRequest;
+import com.example.rent2gojavaproject.services.dtos.requests.userRequest.UpdateUserRequest;
+import com.example.rent2gojavaproject.services.dtos.responses.userResponse.GetUserListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.userResponse.GetUserResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class UserManager implements UserService {
+    private final UserRepository userRepository;
+    private ModelMapperService mapperService;
+    @Override
+    public DataResult<List<GetUserListResponse>> getAllUsers() {
+        List<User> users = this.userRepository.findAll();
+        List<GetUserListResponse> responses = users.stream().map(employee -> this.mapperService.forResponse().map(users, GetUserListResponse.class)).collect(Collectors.toList());
+
+
+        return new SuccessDataResult<List<GetUserListResponse>>(responses,"Transaction Successfully");
+    }
+
+    @Override
+    public DataResult<GetUserResponse> getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Result addUser(AddUserRequest addUserRequest) {
+        return null;
+    }
+
+    @Override
+    public Result updateUser(UpdateUserRequest updateUserRequest) {
+        return null;
+    }
+
+    @Override
+    public Result deleteUser(int id) {
+        return null;
+    }
 }
