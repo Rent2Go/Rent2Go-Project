f24620dd052ab67e59319ecd16c0cf3742f80b54
Soner <syln.soner@gmail.com>
2023-12-16 00:26:27 +0300

    Updated ModelManager

--- src/main/java/com/example/rent2gojavaproject/services/concretes/ModelManager.java
+++ src/main/java/com/example/rent2gojavaproject/services/concretes/ModelManager.java
@@ -1,11 +1,16 @@
 package com.example.rent2gojavaproject.services.concretes;
 
 import com.example.rent2gojavaproject.core.utilities.mappers.ModelMapperService;
+import com.example.rent2gojavaproject.core.utilities.results.DataResult;
+import com.example.rent2gojavaproject.core.utilities.results.Result;
+import com.example.rent2gojavaproject.core.utilities.results.SuccessDataResult;
+import com.example.rent2gojavaproject.core.utilities.results.SuccessResult;
 import com.example.rent2gojavaproject.models.Model;
 import com.example.rent2gojavaproject.repositories.ModelRepository;
 import com.example.rent2gojavaproject.services.abstracts.ModelService;
 import com.example.rent2gojavaproject.services.dtos.requests.modelRequest.AddModelRequest;
 import com.example.rent2gojavaproject.services.dtos.requests.modelRequest.UpdateModelRequest;
+import com.example.rent2gojavaproject.services.dtos.responses.employeeResponse.GetEmployeeListResponse;
 import com.example.rent2gojavaproject.services.dtos.responses.modelResponse.GetModelListResponse;
 import com.example.rent2gojavaproject.services.dtos.responses.modelResponse.GetModelResponse;
 import com.example.rent2gojavaproject.services.rules.ModelBusinessRules;
@@ -21,40 +26,46 @@ public class ModelManager implements ModelService {
     private final ModelRepository modelRepository;
     private ModelMapperService mapperService;
     @Override
-    public List<GetModelListResponse> getAllModels() {
+    public DataResult<List<GetModelListResponse>> getAllModels() {
         List<Model> models = modelRepository.findAll();
         List<GetModelListResponse> responses = models.stream()
                 .map(model -> this.mapperService.forResponse()
                         .map(model,GetModelListResponse.class)).collect(Collectors.toList());
 
-        return responses;
+        return new SuccessDataResult<List<GetModelListResponse>>(responses, "Transaction Successfully");
 
     }
 
     @Override
-    public GetModelResponse getById(int id) {
+    public DataResult<GetModelResponse> getById(int id) {
         Model model = this.modelRepository.findById(id).orElseThrow(() -> new RuntimeException("Couldn't find model id!"));
 
         GetModelResponse response = this.mapperService.forResponse().map(model,GetModelResponse.class);
-        return response;
+        return new SuccessDataResult<GetModelResponse>(response, "Transaction Successfully");
     }
 
     @Override
-    public String addModel(AddModelRequest addModelRequest) {
+    public Result addModel(AddModelRequest addModelRequest){
 
         Model model = this.mapperService.forRequest().map(addModelRequest,Model.class);
         this.modelRepository.save(model);
 
-        return "Transaction Successfully.";
+        return new SuccessResult("Added employee successfully");
     }
 
     @Override
-    public String updateModel(UpdateModelRequest updateModelRequest) {
-        return null;
+    public Result updateModel(UpdateModelRequest updateModelRequest) {
+        Model model = this.modelRepository.findById(updateModelRequest.getId()).orElseThrow(() -> new RuntimeException("Couldn't find model id!"));
+
+        model = this.mapperService.forRequest().map(updateModelRequest, Model.class);
+        this.modelRepository.save(model);
+        return new SuccessResult("Updated employee successfully");
     }
 
     @Override
-    public String deleteModel(int id) {
-        return null;
+    public Result deleteModel(int id) {
+        this.modelRepository.findById(id).orElseThrow(() -> new RuntimeException("Couldn't find model id"));
+        this.modelRepository.deleteById(id);
+        return new SuccessResult("Deleted employee successfully");
     }
 }
