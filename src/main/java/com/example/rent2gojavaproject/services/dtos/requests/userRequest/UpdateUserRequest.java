0e3e6bf079332da1c837af5c306d80dc8133b00d
Soner <syln.soner@gmail.com>
2023-12-16 00:55:23 +0300

    Annotations added

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/UpdateUserRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/UpdateUserRequest.java
@@ -1,5 +1,12 @@
 package com.example.rent2gojavaproject.services.dtos.requests.userRequest;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateUserRequest {
     private int id;
 
