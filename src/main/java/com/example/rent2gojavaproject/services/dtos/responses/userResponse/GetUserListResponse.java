59a1674c7c541e85a22ed24d927b4137a2660184
Soner <syln.soner@gmail.com>
2023-12-16 00:35:56 +0300

    added entities in GetUserListResponse

--- src/main/java/com/example/rent2gojavaproject/services/dtos/responses/userResponse/GetUserListResponse.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/userResponse/GetUserListResponse.java
@@ -1,4 +1,14 @@
 package com.example.rent2gojavaproject.services.dtos.responses.userResponse;
 
+
 public class GetUserListResponse {
+    private int id;
+
+    private String name;
+
+    private String surname;
+
+    private String phoneNumber;
+
+    private String email;
 }
