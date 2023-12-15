d3150f8c21c5528b17d7b6e52a943a01cfb34fe5
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-16 00:25:04 +0300

    created AddDiscountRequest

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/discountRequest/AddDiscountRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/discountRequest/AddDiscountRequest.java
@@ -1,4 +1,22 @@
 package com.example.rent2gojavaproject.services.dtos.requests.discountRequest;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddDiscountRequest {
+    @NotNull(message = "Discount Code can not be empty !")
+    private String discountCode;
+    @NotNull(message = "Percentage can not be empty !")
+    @Positive(message = "Please enter a positive value !!")
+    private double percentage;
+    @NotNull(message = "Rental Id can not be empty !")
+    private int rentalId;
+
+
 }
