388bb06c9fe101dd3edddf7a834fc57c39543449
Soner <syln.soner@gmail.com>
2023-12-15 20:02:33 +0300

    added crud methods in BrandManager & Service

--- src/main/java/com/example/rent2gojavaproject/services/abstracts/BrandService.java
+++ src/main/java/com/example/rent2gojavaproject/services/abstracts/BrandService.java
@@ -1,4 +1,20 @@
 package com.example.rent2gojavaproject.services.abstracts;
 
+import com.example.rent2gojavaproject.services.dtos.requests.brandRequest.AddBrandRequest;
+import com.example.rent2gojavaproject.services.dtos.requests.brandRequest.UpdateBrandRequest;
+import com.example.rent2gojavaproject.services.dtos.responses.brandResponse.GetBrandListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.brandResponse.GetBrandResponse;
+
+import java.util.List;
+
 public interface BrandService {
+    List<GetBrandListResponse> getAllBrands();
+
+    GetBrandResponse getById(int id);
+
+    String addBrand(AddBrandRequest addBrandRequest);
+
+    String updateBrand(UpdateBrandRequest updateBrandRequest);
+
+    String deleteBrand(int id);
 }
--- src/main/java/com/example/rent2gojavaproject/services/concretes/BrandManager.java
+++ src/main/java/com/example/rent2gojavaproject/services/concretes/BrandManager.java
@@ -1,4 +1,46 @@
 package com.example.rent2gojavaproject.services.concretes;
 
-public class BrandManager {
+import com.example.rent2gojavaproject.core.utilities.mappers.ModelMapperService;
+import com.example.rent2gojavaproject.repositories.BrandRepository;
+import com.example.rent2gojavaproject.services.abstracts.BrandService;
+import com.example.rent2gojavaproject.services.dtos.requests.brandRequest.AddBrandRequest;
+import com.example.rent2gojavaproject.services.dtos.requests.brandRequest.UpdateBrandRequest;
+import com.example.rent2gojavaproject.services.dtos.responses.brandResponse.GetBrandListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.brandResponse.GetBrandResponse;
+import com.example.rent2gojavaproject.services.rules.BrandBusinessRules;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+public class BrandManager implements BrandService {
+    private final BrandRepository brandRepository;
+    private ModelMapperService mapperService;
+    private BrandBusinessRules businessRules;
+
+    @Override
+    public List<GetBrandListResponse> getAllBrands() {
+        return null;
+    }
+
+    @Override
+    public GetBrandResponse getById(int id) {
+        return null;
+    }
+
+    @Override
+    public String addBrand(AddBrandRequest addBrandRequest) {
+        return null;
+    }
+
+    @Override
+    public String updateBrand(UpdateBrandRequest updateBrandRequest) {
+        return null;
+    }
+
+    @Override
+    public String deleteBrand(int id) {
+        return null;
+    }
 }
