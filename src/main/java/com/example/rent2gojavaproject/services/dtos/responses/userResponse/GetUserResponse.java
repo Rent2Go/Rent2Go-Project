0e71e707af20c1736dc90c507347395aef6d55a7
Soner <syln.soner@gmail.com>
2023-12-16 00:55:01 +0300

    Annotations added

--- src/main/java/com/example/rent2gojavaproject/services/dtos/responses/userResponse/GetUserResponse.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/userResponse/GetUserResponse.java
@@ -1,5 +1,12 @@
 package com.example.rent2gojavaproject.services.dtos.responses.userResponse;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetUserResponse {
     private int id;
 
