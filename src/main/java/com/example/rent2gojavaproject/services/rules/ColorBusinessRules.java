d20770d8373d6ece19b52978a313495b78a8005c
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-15 17:42:54 +0300

    created checkIfExistsById method for Rental

--- src/main/java/com/example/rent2gojavaproject/services/rules/ColorBusinessRules.java
+++ src/main/java/com/example/rent2gojavaproject/services/rules/ColorBusinessRules.java
@@ -1,4 +1,11 @@
 package com.example.rent2gojavaproject.services.rules;
 
+import com.example.rent2gojavaproject.repositories.ColorRepository;
+
 public class ColorBusinessRules {
+
+    private final ColorRepository colorRepository;
+
+
+
 }
--- src/main/java/com/example/rent2gojavaproject/services/rules/RentalBusinessRules.java
+++ src/main/java/com/example/rent2gojavaproject/services/rules/RentalBusinessRules.java
@@ -1,4 +1,30 @@
 package com.example.rent2gojavaproject.services.rules;
 
+import com.example.rent2gojavaproject.repositories.CarRepository;
+import com.example.rent2gojavaproject.repositories.CustomerRepository;
+import com.example.rent2gojavaproject.repositories.EmployeeRepository;
+import com.example.rent2gojavaproject.repositories.RentalRepository;
+
 public class RentalBusinessRules {
+
+    private CarRepository carRepository;
+    private RentalRepository rentalRepository;
+    private CustomerRepository customerRepository;
+    private EmployeeRepository employeeRepository;
+
+    public void checkIfExistsById(int carId, int customerId, int employeeId) {
+        if (!(carRepository.existsById(carId))) {
+            throw new IllegalStateException("Car ID doesn't exist !");
+        }
+        else if (!(customerRepository.existsById(customerId))) {
+            throw new IllegalStateException("Customer ID doesn't exist !");
+        }
+        else if (!(employeeRepository.existsById(employeeId))) {
+            throw new IllegalStateException("Employee ID doesn't exist !");
+        }
+
+    }
+
+
+
 }
