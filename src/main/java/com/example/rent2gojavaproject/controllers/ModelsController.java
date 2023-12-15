df5ab3086013b62dfb489019df628c41baa58cfb
Soner <syln.soner@gmail.com>
2023-12-16 00:26:56 +0300

    Added Controler methods in ModelsController

--- src/main/java/com/example/rent2gojavaproject/controllers/ModelsController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/ModelsController.java
@@ -1,9 +1,50 @@
 package com.example.rent2gojavaproject.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.rent2gojavaproject.core.utilities.results.DataResult;
+import com.example.rent2gojavaproject.core.utilities.results.Result;
+import com.example.rent2gojavaproject.services.abstracts.ModelService;
+import com.example.rent2gojavaproject.services.dtos.requests.modelRequest.AddModelRequest;
+import com.example.rent2gojavaproject.services.dtos.requests.modelRequest.UpdateModelRequest;
+import com.example.rent2gojavaproject.services.dtos.responses.modelResponse.GetModelListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.modelResponse.GetModelResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/V1/models")
+@AllArgsConstructor
 public class ModelsController {
+    private final ModelService modelService;
+
+    @GetMapping("/getall")
+    public DataResult<List<GetModelListResponse>> getAllModels() {
+        return modelService.getAllModels();
+    }
+
+    @GetMapping("/{id}")
+    public DataResult<GetModelResponse> getModelById(@PathVariable int id) {
+        return modelService.getById(id);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Result createModel(@RequestBody @Valid AddModelRequest addModelRequest) {
+        return modelService.addModel(addModelRequest);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Result updateModel(@RequestBody @Valid UpdateModelRequest updateModelRequest) {
+        return modelService.updateModel(updateModelRequest);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Result deleteModel(@PathVariable int id) {
+        return modelService.deleteModel(id);
+    }
 }
