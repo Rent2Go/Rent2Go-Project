9984be052c5497ee003f01bf6eeaae445e865818
ShmsErl <erol.sehmus1@gmail.com>
2023-12-15 21:22:34 +0300

    DataResult class created.

---
+++ src/main/java/com/example/rent2gojavaproject/core/utilities/results/DataResult.java
@@ -0,0 +1,23 @@
+package com.example.rent2gojavaproject.core.utilities.results;
+
+public class DataResult<T> extends Result {
+
+
+    private T data;
+
+    public DataResult() {
+    }
+
+    public DataResult(T data, String message, boolean result) {
+        super(message, result);
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
