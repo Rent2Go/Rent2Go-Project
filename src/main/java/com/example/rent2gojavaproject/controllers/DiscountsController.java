bab8fc523d2d0b918b4a09500e8b054000593cb4
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-16 01:34:08 +0300

    created Discount Controller

--- src/main/java/com/example/rent2gojavaproject/controllers/DiscountsController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/DiscountsController.java
@@ -1,9 +1,44 @@
 package com.example.rent2gojavaproject.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.rent2gojavaproject.core.utilities.results.DataResult;
+import com.example.rent2gojavaproject.core.utilities.results.Result;
+import com.example.rent2gojavaproject.services.abstracts.DiscountService;
+import com.example.rent2gojavaproject.services.dtos.requests.discountRequest.AddDiscountRequest;
+import com.example.rent2gojavaproject.services.dtos.requests.discountRequest.UpdateDiscountRequest;
+import com.example.rent2gojavaproject.services.dtos.responses.discountResponse.GetDiscountListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.discountResponse.GetDiscountResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.employeeResponse.GetEmployeeListResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.crypto.Data;
+import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/V1/discounts")
 public class DiscountsController {
+    private final DiscountService discountService;
+
+    @GetMapping("/getAll")
+    public DataResult<List<GetDiscountListResponse>> getAllDiscounts(){
+        return discountService.getAllDiscounts();
+    }
+
+    @GetMapping("/{id}")
+    public DataResult<GetDiscountResponse> getDiscountById(@PathVariable int id){
+        return discountService.getById(id);
+    }
+
+    @PostMapping("/add")
+    public Result createDiscount(@RequestBody @Valid AddDiscountRequest addDiscountRequest){
+        return discountService.addDiscount(addDiscountRequest);
+    }
+
+    @PutMapping("/update")
+    public Result updateDiscount(@RequestBody @Valid UpdateDiscountRequest updateDiscountRequest){
+        return discountService.updateDiscount(updateDiscountRequest);
+    }
 }
