7dcc69f41df76100ab526255fdf7b6f5055fa3ea
ShmsErl <erol.sehmus1@gmail.com>
2023-12-16 01:01:44 +0300

    bug fix

--- src/main/java/com/example/rent2gojavaproject/services/rules/ColorBusinessRules.java
+++ src/main/java/com/example/rent2gojavaproject/services/rules/ColorBusinessRules.java
@@ -1,7 +1,9 @@
 package com.example.rent2gojavaproject.services.rules;
 
 import com.example.rent2gojavaproject.repositories.ColorRepository;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class ColorBusinessRules {
 
     private final ColorRepository colorRepository;
