41738ee142320c18dd0ee43699c84466aedfd217
Soner <syln.soner@gmail.com>
2023-12-16 00:59:12 +0300

    added getById

--- src/main/java/com/example/rent2gojavaproject/controllers/UsersController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/UsersController.java
@@ -3,8 +3,10 @@ package com.example.rent2gojavaproject.controllers;
 import com.example.rent2gojavaproject.core.utilities.results.DataResult;
 import com.example.rent2gojavaproject.services.abstracts.UserService;
 import com.example.rent2gojavaproject.services.dtos.responses.userResponse.GetUserListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.userResponse.GetUserResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class UsersController {
     @GetMapping("/getall")
     public DataResult<List<GetUserListResponse>> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public DataResult<GetUserResponse> getUserById(@PathVariable int id) {
+        return userService.getById(id);
     }
 }
