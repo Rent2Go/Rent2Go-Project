58a11f242ce5a2c07c7e7e48440988bec35faea3
Yagmur Curku <yagmurcurku@gmail.com>
2023-12-16 23:26:25 +0300

    created ColorBusinessRules

--- src/main/java/com/example/rent2gojavaproject/services/rules/ColorBusinessRules.java
+++ src/main/java/com/example/rent2gojavaproject/services/rules/ColorBusinessRules.java
@@ -1,11 +1,19 @@
 package com.example.rent2gojavaproject.services.rules;
 
+import com.example.rent2gojavaproject.repositories.ColorRepository;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class ColorBusinessRules {
 
 
+    private ColorRepository colorRepository;
+
+    public void checkIfExistsByName(String name){
+        if(colorRepository.existsByName(name.toLowerCase().trim())){
+            throw new IllegalArgumentException("Color already exists");
+        }
+    }
 
 
 }
