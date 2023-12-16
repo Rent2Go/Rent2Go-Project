89f0735059f48f15cfbc5dc8c7f28bf8cd64b771
ShmsErl <erol.sehmus1@gmail.com>
2023-12-16 22:08:58 +0300

    Add Enum messages.

--- src/main/java/com/example/rent2gojavaproject/core/utilities/alerts/Message.java
+++ src/main/java/com/example/rent2gojavaproject/core/utilities/alerts/Message.java
@@ -3,6 +3,22 @@ package com.example.rent2gojavaproject.core.utilities.alerts;
 
 public enum Message {
 
+    ADD("The addition operation was successful!"),
+    UPDATE("The update operation was successful!"),
+    DELETE("The deletion operation was successful!"),
+    GET_ALL("All records were listed!"),
+    GET("The record was found!");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
 
 
 }
