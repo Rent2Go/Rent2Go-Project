911ab299ac5a125abcfdcfc77c1f50b039645917
Soner <syln.soner@gmail.com>
2023-12-17 22:40:55 +0300

    added regexp in Add&UpdateDiscountRequests

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/discountRequest/AddDiscountRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/discountRequest/AddDiscountRequest.java
@@ -1,6 +1,8 @@
 package com.example.rent2gojavaproject.services.dtos.requests.discountRequest;
 
+import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,10 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AddDiscountRequest {
     @NotNull(message = "Discount Code can not be empty !")
+    @Pattern(regexp = "^[A-Z0-9]+$", message = "Discount code must consist of uppercase letters and numbers only.")
     private String discountCode;
     @NotNull(message = "Percentage can not be empty !")
     @Positive(message = "Please enter a positive value !!")
-    private double percentage;
+    @DecimalMax(value = "0.99", message = "Please enter a value less than or equal to 0.99")
+    private Double percentage;
     @NotNull(message = "Rental Id can not be empty !")
     private int rentalId;
 
--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/discountRequest/UpdateDiscountRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/discountRequest/UpdateDiscountRequest.java
@@ -1,6 +1,8 @@
 package com.example.rent2gojavaproject.services.dtos.requests.discountRequest;
 
+import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,8 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateDiscountRequest {
+    @NotNull(message = "Discount ID must be entered !!")
+    @Positive(message = "Discount ID must be bigger than 0 !!")
     private int id;
+    @NotNull(message = "Discount Code can not be empty !")
+    @Pattern(regexp = "^[A-Z0-9]+$", message = "Discount code must consist of uppercase letters and numbers only.")
     private String discountCode;
-    private double percentage;
+    @NotNull(message = "Percentage can not be empty !")
+    @Positive(message = "Please enter a positive value !!")
+    @DecimalMax(value = "0.99", message = "Please enter a value less than or equal to 0.99")
+    private Double percentage;
+    @NotNull(message = "Rental Id can not be empty !")
     private int rentalId;
 }
