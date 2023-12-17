897916aef58fcad1e176e4c8d8f11d80b117faa7
Soner <syln.soner@gmail.com>
2023-12-17 22:40:12 +0300

    Add findByDiscountCode in DiscountRepository

--- src/main/java/com/example/rent2gojavaproject/repositories/DiscountRepository.java
+++ src/main/java/com/example/rent2gojavaproject/repositories/DiscountRepository.java
@@ -4,5 +4,6 @@ import com.example.rent2gojavaproject.models.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiscountRepository extends JpaRepository<Discount,Integer> {
+    Discount findByDiscountCode(String discountCode);
 
 }
