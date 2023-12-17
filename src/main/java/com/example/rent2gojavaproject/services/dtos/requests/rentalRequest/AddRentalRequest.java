ceef8f8be333decdeea3bd0cd66349f65a8dc07e
Soner <syln.soner@gmail.com>
2023-12-17 22:41:34 +0300

    update regexp method in Add&UpdateRentalRequests

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/rentalRequest/AddRentalRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/rentalRequest/AddRentalRequest.java
@@ -31,8 +31,7 @@ public class AddRentalRequest {
     @NotNull(message = "Employee ID must be entered !!")
     private int employeeId;
 
-    @NotNull(message = "Discount ID must be entered !!")
-    @Pattern(regexp = "^[A-Z0-9]+$", message = "Discount code must consist of uppercase letters and numbers only.")
+    @Pattern(regexp = "^$|^[A-Z0-9]+$", message = "Discount code must consist of uppercase letters and numbers only.")
     private String discountCode;
 
 
--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/rentalRequest/UpdateRentalRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/rentalRequest/UpdateRentalRequest.java
@@ -36,7 +36,6 @@ public class UpdateRentalRequest {
     @NotNull(message = "Employee ID must be entered !!")
     private int employeeId;
 
-    @NotNull(message = "Discount ID must be entered !!")
     @Pattern(regexp = "^[A-Z0-9]+$", message = "Discount code must consist of uppercase letters and numbers only.")
     private String discountCode;
 
