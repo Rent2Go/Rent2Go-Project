f9266171e6c1bd9c4fab1beb677e4535adc0fc95
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-18 23:38:13 +0300

    created Bill Model

---
+++ src/main/java/com/example/rent2gojavaproject/models/Bill.java
@@ -0,0 +1,49 @@
+package com.example.rent2gojavaproject.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jdk.dynalink.linker.LinkerServices;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "bills")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "date", nullable = false)
+    private double price;
+
+    @Column(name = "is_active", nullable = false, columnDefinition = "integer default 1")
+    private int isActive;
+
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "integer default 0")
+    private int isDeleted;
+
+    @Column(name = "created_at" , nullable = false)
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @OneToMany(mappedBy = "bill")
+    @JsonIgnore
+    private List<User> users;
+
+
+}
