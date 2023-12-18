a684428c13e5a832d38e147eb7472a469cae2783
ShmsErl <erol.sehmus1@gmail.com>
2023-12-18 22:20:06 +0300

    Service classes were injected instead of repositories.

--- src/main/java/com/example/rent2gojavaproject/services/rules/CarBusinessRules.java
+++ src/main/java/com/example/rent2gojavaproject/services/rules/CarBusinessRules.java
@@ -5,21 +5,22 @@ import com.example.rent2gojavaproject.repositories.ColorRepository;
 import com.example.rent2gojavaproject.repositories.ModelRepository;
 import com.example.rent2gojavaproject.services.abstracts.CarService;
 import com.example.rent2gojavaproject.services.abstracts.ColorService;
+import com.example.rent2gojavaproject.services.abstracts.ModelService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class CarBusinessRules {
-    private final ModelRepository modelRepository;
+    private final ModelService modelService;
     private final ColorService colorService;
-    private final CarService carService;
+    private final CarRepository carRepository;
 
 
     public String plateUniqueness (String plate){
 
         String licensePlate = plate.replace(" ", "").toUpperCase();
-        boolean result = this.carService.existsByPlate(licensePlate);
+        boolean result = this.carRepository.existsByPlate(licensePlate);
         if (result) {
             throw new IllegalArgumentException("Car Plate already exists! : " + plate);
         }
@@ -28,7 +29,7 @@ public class CarBusinessRules {
     }
 
     public void updateCarMethod (int modelId, int colorId){
-        if (!(modelRepository.existsById(modelId) && colorService.existsById(colorId))){
+        if (!(modelService.existsById(modelId) && colorService.existsById(colorId))){
             throw  new IllegalStateException("Model or color does not exist");
         }
     }
