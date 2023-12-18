541a64cf907214a40d7c69cd6b45b066a4339f8c
Soner <syln.soner@gmail.com>
2023-12-18 12:05:40 +0300

    updated Add&UpdateCustomerRequests

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/customerRequest/AddCustomerRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/customerRequest/AddCustomerRequest.java
@@ -1,5 +1,9 @@
 package com.example.rent2gojavaproject.services.dtos.requests.customerRequest;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AddCustomerRequest {
 
+    @NotBlank(message = "Nationality ID cannot be blank")
+    @Pattern(regexp = "[0-9]{11}", message = "Nationality ID must be 11 digits numeric")
     private String nationalityId;
-
+    @NotNull(message = "The user id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int userId;
 }
--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/customerRequest/UpdateCustomerRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/customerRequest/UpdateCustomerRequest.java
@@ -1,5 +1,9 @@
 package com.example.rent2gojavaproject.services.dtos.requests.customerRequest;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +12,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateCustomerRequest {
+    @NotNull(message = "The customer id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int id;
 
+    @NotBlank(message = "Nationality ID cannot be blank")
+    @Pattern(regexp = "[0-9]{11}", message = "Nationality ID must be 11 digits numeric")
     private String nationalityId;
-
+    @NotNull(message = "The user id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int userId;
 }
