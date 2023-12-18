c99685c1d1e0afd5dca2372ae29bb53a6ab72085
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-18 23:57:24 +0300

    created BodyType, Engine, FuelType, Gear models

---
+++ src/main/java/com/example/rent2gojavaproject/models/Engine.java
@@ -0,0 +1,28 @@
+package com.example.rent2gojavaproject.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "engines")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Engine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "engine", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Car> cars;
+}
---
+++ src/main/java/com/example/rent2gojavaproject/models/Gear.java
@@ -0,0 +1,29 @@
+package com.example.rent2gojavaproject.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "gears")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Gear {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "gear", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Car> cars;
+}
