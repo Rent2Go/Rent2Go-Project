96b096d391b6cd496416e39277103459fb1b4bd7
ShmsErl <erol.sehmus1@gmail.com>
2023-12-18 21:33:48 +0300

    Service classes were injected instead of repositories.

--- src/main/java/com/example/rent2gojavaproject/services/rules/ModelBusinessRules.java
+++ src/main/java/com/example/rent2gojavaproject/services/rules/ModelBusinessRules.java
@@ -2,6 +2,7 @@ package com.example.rent2gojavaproject.services.rules;
 
 import com.example.rent2gojavaproject.repositories.BrandRepository;
 import com.example.rent2gojavaproject.repositories.ModelRepository;
+import com.example.rent2gojavaproject.services.abstracts.BrandService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +11,11 @@ import org.springframework.stereotype.Service;
 public class ModelBusinessRules {
 
     private ModelRepository modelRepository;
-    private BrandRepository brandRepository;
+    private BrandService brandService;
 
     public String checkIfExistsByIdAndName(int brandId, String name){
         String value = name.toLowerCase().trim();
-        if (!(brandRepository.existsById(brandId))) {
+        if (!(brandService.existsById(brandId))) {
             throw new IllegalStateException("Brand ID doesn't exist !");
         }else if(modelRepository.existsByName(value)){
             throw new IllegalArgumentException("Model already exists");
