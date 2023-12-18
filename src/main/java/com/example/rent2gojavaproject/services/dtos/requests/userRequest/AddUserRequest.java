a877de50e3e4a8922e70c90f57cb090c91b5e585
Soner <syln.soner@gmail.com>
2023-12-18 12:11:57 +0300

    added validation rules in Add&UpdateUserRequests

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/AddUserRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/AddUserRequest.java
@@ -1,5 +1,9 @@
 package com.example.rent2gojavaproject.services.dtos.requests.userRequest;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +12,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddUserRequest {
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 20, message = "Name must be between 2 and 20 characters")
     private String name;
 
+    @NotBlank(message = "Surname cannot be blank")
+    @Size(min = 2, max = 20, message = "Surname must be between 2 and 20 characters")
     private String surname;
-
+    @NotBlank(message = "Phone number cannot be blank")
+    @Pattern(regexp = "05[0-9]{9}", message = "Invalid phone number format. It must be in the format 05xxxxxxxxx.")
     private String phoneNumber;
 
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
     private String email;
 }
--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/UpdateUserRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/UpdateUserRequest.java
@@ -1,5 +1,6 @@
 package com.example.rent2gojavaproject.services.dtos.requests.userRequest;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,13 +9,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateUserRequest {
+    @NotNull(message = "The user id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int id;
-
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 20, message = "Name must be between 2 and 20 characters")
     private String name;
 
+    @NotBlank(message = "Surname cannot be blank")
+    @Size(min = 2, max = 20, message = "Surname must be between 2 and 20 characters")
     private String surname;
-
+    @NotBlank(message = "Phone number cannot be blank")
+    @Pattern(regexp = "05[0-9]{9}", message = "Invalid phone number format. It must be in the format 05xxxxxxxxx.")
     private String phoneNumber;
 
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
     private String email;
 }
