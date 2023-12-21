a8e8a3d7d3d7d5605c80c76f3d4d64ae4e859fcc
ShmsErl <erol.sehmus1@gmail.com>
2023-12-21 13:47:19 +0300

    Exception classes have been updated in BrandBusinessRule.

--- src/main/java/com/example/rent2gojavaproject/services/rules/BrandBusinessRules.java
+++ src/main/java/com/example/rent2gojavaproject/services/rules/BrandBusinessRules.java
@@ -1,8 +1,7 @@
 package com.example.rent2gojavaproject.services.rules;
 
-import com.example.rent2gojavaproject.models.Brand;
+import com.example.rent2gojavaproject.core.exceptions.AlreadyExistsException;
 import com.example.rent2gojavaproject.repositories.BrandRepository;
-import com.example.rent2gojavaproject.repositories.ColorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +14,7 @@ public class BrandBusinessRules {
     public String checkIfExistsByName(String name){
         String value = name.toLowerCase().trim();
         if(brandRepository.existsByNameAndIsActiveTrue(value)){
-            throw new IllegalArgumentException("Brand already exists");
+            throw new AlreadyExistsException("Brand already exists");
         }
         return value;
     }
