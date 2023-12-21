dc1cd730b62045a6622de77b0bbe44bdd1675088
ShmsErl <erol.sehmus1@gmail.com>
2023-12-21 13:24:42 +0300

    Created Global Business Rules error class created.

---
+++ src/main/java/com/example/rent2gojavaproject/core/exceptions/BusinessRuleException.java
@@ -0,0 +1,8 @@
+package com.example.rent2gojavaproject.core.exceptions;
+
+public class BusinessRuleException extends Exception {
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
