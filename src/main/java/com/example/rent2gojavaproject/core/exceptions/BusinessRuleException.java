de56fcc2eeab9bbd36e0450a516317ddae726fd9
ShmsErl <erol.sehmus1@gmail.com>
2023-12-21 13:51:59 +0300

    Update excepiton class.

--- src/main/java/com/example/rent2gojavaproject/core/exceptions/BusinessRuleException.java
+++ src/main/java/com/example/rent2gojavaproject/core/exceptions/BusinessRuleException.java
@@ -1,6 +1,6 @@
 package com.example.rent2gojavaproject.core.exceptions;
 
-public class BusinessRuleException extends Exception {
+public class BusinessRuleException extends RuntimeException {
 
     public BusinessRuleException(String message) {
         super(message);
