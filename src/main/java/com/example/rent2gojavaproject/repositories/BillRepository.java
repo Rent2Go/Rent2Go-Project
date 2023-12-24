eb39bae5f1f0ec8977001b9fca70d8df6a52a8a5
Soner <syln.soner@gmail.com>
2023-12-25 00:16:20 +0300

    filled all packages for Bill

--- src/main/java/com/example/rent2gojavaproject/repositories/BillRepository.java
+++ src/main/java/com/example/rent2gojavaproject/repositories/BillRepository.java
@@ -1,2 +1,7 @@
-package com.example.rent2gojavaproject.repositories;public interface BillRepository {
+package com.example.rent2gojavaproject.repositories;
+
+import com.example.rent2gojavaproject.models.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BillRepository extends JpaRepository <Bill, Integer> {
 }
--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/billRequest/UpdateBillRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/billRequest/UpdateBillRequest.java
@@ -1,2 +1,30 @@
-package com.example.rent2gojavaproject.services.dtos.requests.billRequest;public class UpdateBillRequest {
+package com.example.rent2gojavaproject.services.dtos.requests.billRequest;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateBillRequest {
+    @Positive(message = "ID should be a positive integer")
+    private int id;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotNull(message = "Date cannot be null")
+    private LocalDate date;
+
+    @Positive(message = "Price should be a positive number")
+    private double price;
+
+    @Positive(message = "UserID should be a positive integer")
+    private int userId;
 }
