d134f03be1871265b70c24bcb8e85456cb2e4aee
ShmsErl <erol.sehmus1@gmail.com>
2023-12-30 19:42:28 +0300

    Created findByEmail querry .

--- src/main/java/com/example/rent2gojavaproject/repositories/UserRepository.java
+++ src/main/java/com/example/rent2gojavaproject/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package com.example.rent2gojavaproject.repositories;
 import com.example.rent2gojavaproject.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByEmail(String email);
 
 }
