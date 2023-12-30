e02c7932cb5b8faaed2586e85e35f603dd7ba59c
ShmsErl <erol.sehmus1@gmail.com>
2023-12-30 19:44:05 +0300

    Created Role enum entity.

---
+++ src/main/java/com/example/rent2gojavaproject/models/Role.java
@@ -0,0 +1,15 @@
+package com.example.rent2gojavaproject.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public enum Role{
+
+    ROLE_ADMIN(),
+    ROLE_USER
+
+
+}
