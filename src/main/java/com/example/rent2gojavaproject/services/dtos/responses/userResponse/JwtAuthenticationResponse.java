40e2e94f329cee1350426fe3ba0145591f66938b
ShmsErl <erol.sehmus1@gmail.com>
2023-12-30 19:45:53 +0300

    Created JwtAuthenticationResponse  dto class.

---
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/userResponse/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.rent2gojavaproject.services.dtos.responses.userResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+    String token;
+}
