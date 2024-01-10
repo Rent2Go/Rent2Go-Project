fb445fec003ebd52ce3fe7879996d1691c30277a
Soner <syln.soner@gmail.com>
2024-01-10 17:06:03 +0300

    added District entity

--- src/main/java/com/example/rent2gojavaproject/models/District.java
+++ src/main/java/com/example/rent2gojavaproject/models/District.java
@@ -1,2 +1,33 @@
-package com.example.rent2gojavaproject.models;public class District {
+package com.example.rent2gojavaproject.models;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "districts")
+@SQLDelete(sql = "update districts SET IS_ACTIVE = false WHERE id=?")
+//@Where(clause = "IS_ACTIVE=true")
+@FilterDef(name="isActiveFilterDistrict", parameters=@ParamDef( name="isActive", type=Boolean.class ))
+@Filter(name="isActiveFilterDistrict", condition="IS_ACTIVE = :isActive")
+public class District extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "disctrict_name")
+    private String districtName;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
 }
