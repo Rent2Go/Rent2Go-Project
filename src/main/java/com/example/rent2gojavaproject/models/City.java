cbd704f7cd382431ed8e4f7f1068d43f976089b3
Soner <syln.soner@gmail.com>
2024-01-10 17:05:52 +0300

    added City entity

--- src/main/java/com/example/rent2gojavaproject/models/City.java
+++ src/main/java/com/example/rent2gojavaproject/models/City.java
@@ -1,100 +1,36 @@
 package com.example.rent2gojavaproject.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
 
-public enum City {
-    ADANA("Adana"),
-    ADIYAMAN("Adiyaman"),
-    AFYONKARAHISAR("Afyonkarahisar"),
-    AGRI("Agri"),
-    AKSARAY("Aksaray"),
-    AMASYA("Amasya"),
-    ANKARA("Ankara"),
-    ANTALYA("Antalya"),
-    ARDAHAN("Ardahan"),
-    ARTVIN("Artvin"),
-    AYDIN("Aydin"),
-    BALIKESIR("Balikesir"),
-    BARTIN("Bartin"),
-    BATMAN("Batman"),
-    BAYBURT("Bayburt"),
-    BILECIK("Bilecik"),
-    BINGOL("Bingol"),
-    BITLIS("Bitlis"),
-    BOLU("Bolu"),
-    BURDUR("Burdur"),
-    BURSA("Bursa"),
-    CANAKKALE("Canakkale"),
-    CANKIRI("Cankiri"),
-    CORUM("Corum"),
-    DENIZLI("Denizli"),
-    DIYARBAKIR("Diyarbakir"),
-    DUZCE("Duzce"),
-    EDIRNE("Edirne"),
-    ELAZIG("Elazig"),
-    ERZINCAN("Erzincan"),
-    ERZURUM("Erzurum"),
-    ESKISEHIR("Eskisehir"),
-    GAZIANTEP("Gaziantep"),
-    GIRESUN("Giresun"),
-    GUMUSHANE("Gumushane"),
-    HAKKARI("Hakkari"),
-    HATAY("Hatay"),
-    IGDIR("Igdir"),
-    ISPARTA("Isparta"),
-    ISTANBUL("Istanbul"),
-    IZMIR("Izmir"),
-    KAHIRAMANMARAS("Kahramanmaras"),
-    KARABUK("Karabuk"),
-    KARAMAN("Karaman"),
-    KARS("Kars"),
-    KASTAMONU("Kastamonu"),
-    KAYSERI("Kayseri"),
-    KIRIKKALE("Kirikkale"),
-    KIRKLARELI("Kirklareli"),
-    KIRSEHIR("Kirsehir"),
-    KOCAELI("Kocaeli"),
-    KONYA("Konya"),
-    KUTAHYA("Kutahya"),
-    MALATYA("Malatya"),
-    MANISA("Manisa"),
-    MARDIN("Mardin"),
-    MERSIN("Mersin"),
-    MUGLA("Mugla"),
-    MUS("Mus"),
-    NEVSEHIR("Nevsehir"),
-    NIGDE("Nigde"),
-    ORDU("Ordu"),
-    OSMANIYE("Osmaniye"),
-    RIZE("Rize"),
-    SAKARYA("Sakarya"),
-    SAMSUN("Samsun"),
-    SIIRT("Siirt"),
-    SINOP("Sinop"),
-    SIVAS("Sivas"),
-    SANLIURFA("Sanliurfa"),
-    SIRNAK("Sirnak"),
-    TEKIRDAG("Tekirdag"),
-    TOKAT("Tokat"),
-    TRABZON("Trabzon"),
-    TUNCELI("Tunceli"),
-    USAK("Usak"),
-    VAN("Van"),
-    YALOVA("Yalova"),
-    YOZGAT("Yozgat"),
-    ZONGULDAK("Zonguldak");
+import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cities")
+@SQLDelete(sql = "update cities SET IS_ACTIVE = false WHERE id=?")
+//@Where(clause = "IS_ACTIVE=true")
+@FilterDef(name="isActiveFilterCity", parameters=@ParamDef( name="isActive", type=Boolean.class ))
+@Filter(name="isActiveFilterCity", condition="IS_ACTIVE = :isActive")
+public class City extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-    private final String cityName;
+    @Column(name = "city_name")
+    private String cityName;
 
-    City(String cityName) {
-        this.cityName = cityName;
-    }
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<District> districts;
 
-    public String getCityName() {
-        return cityName;
-    }
 }
---
+++ src/main/java/com/example/rent2gojavaproject/models/District.java
@@ -0,0 +1,2 @@
+package com.example.rent2gojavaproject.models;public class District {
+}
