c8b3fec46ef2fb23ad3903eab756c986839bea3f
ShmsErl <erol.sehmus1@gmail.com>
2024-01-11 21:58:21 +0300

    Added Validation anatations .

--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/SignInRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/SignInRequest.java
@@ -1,5 +1,7 @@
 package com.example.rent2gojavaproject.services.dtos.requests.userRequest;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +12,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignInRequest {
+    @Email
     String email;
+    /*@Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=.*])(?=\\S+$).{8,}"
+            ,message = "En az 8 karakter\n" +
+            "\n" +
+            "En az bir rakam içerir\n" +
+            "\n" +
+            "En az bir alt alfa karakteri ve bir üst alfa karakteri içerir\n" +
+            "\n" +
+            "Bir dizi özel karakter ( @#%$^.*vb.) içinde en az bir karakter içerir.\n" +
+            "\n" +
+            "Boşluk, sekme vb. içermez.")*/
     String password;
 }
--- src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/SignUpRequest.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/userRequest/SignUpRequest.java
@@ -1,5 +1,7 @@
 package com.example.rent2gojavaproject.services.dtos.requests.userRequest;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +15,17 @@ public class SignUpRequest {
     String firstName;
     String lastName;
     String phoneNumber;
+    @Email()
     String email;
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=.*])(?=\\S+$).{8,}"
+            ,message = "En az 8 karakter\n" +
+            "\n" +
+            "En az bir rakam içerir\n" +
+            "\n" +
+            "En az bir alt alfa karakteri ve bir üst alfa karakteri içerir\n" +
+            "\n" +
+            "Bir dizi özel karakter ( @#%$^.*vb.) içinde en az bir karakter içerir.\n" +
+            "\n" +
+            "Boşluk, sekme vb. içermez.")
     String password;
 }
