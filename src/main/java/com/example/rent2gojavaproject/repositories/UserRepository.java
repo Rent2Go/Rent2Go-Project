c7541fb0122792e1f0684a8af29dd3b5ef0aa07e
ShmsErl <erol.sehmus1@gmail.com>
2024-01-11 21:48:28 +0300

    Added Derived method.

--- src/main/java/com/example/rent2gojavaproject/repositories/UserRepository.java
+++ src/main/java/com/example/rent2gojavaproject/repositories/UserRepository.java
@@ -9,4 +9,7 @@ public interface UserRepository extends JpaRepository<User,Integer> {
 
     Optional<User> findByEmail(String email);
 
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+
 }
