0084e347f3573441dbc4086a9b2cdb382446a4f3
ShmsErl <erol.sehmus1@gmail.com>
2024-01-12 13:07:31 +0300

    Created ImageData class.

---
+++ src/main/java/com/example/rent2gojavaproject/models/ImageData.java
@@ -0,0 +1,26 @@
+package com.example.rent2gojavaproject.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name ="images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageData {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    private String type;
+    @Lob
+    @Column(name = "image_data", length = 1000)
+    private byte[] imageData;
+
+}
