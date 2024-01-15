5202484bba1515937124c2dc5a6713e8afd9559f
ShmsErl <erol.sehmus1@gmail.com>
2024-01-15 17:45:53 +0300

    Created endpoints to connect with cloud storage

---
+++ src/main/java/com/example/rent2gojavaproject/controllers/FileUploadController.java
@@ -0,0 +1,28 @@
+package com.example.rent2gojavaproject.controllers;
+
+import com.example.rent2gojavaproject.services.abstracts.FileUpload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/V1/files")
+
+public class FileUploadController {
+
+    private final FileUpload fileUpload;
+
+
+
+    @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public String uploadFile(@RequestParam("image") MultipartFile multipartFile
+                             ) throws IOException {
+        String imageURL = fileUpload.uploadFile(multipartFile);
+        return imageURL;
+    }
+}
