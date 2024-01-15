1633f64d59c28b44299395cd0ad1bf409741615c
ShmsErl <erol.sehmus1@gmail.com>
2024-01-15 17:45:10 +0300

    FileUploadManager The contents of the methods were filled

---
+++ src/main/java/com/example/rent2gojavaproject/services/concretes/FileUploadManager.java
@@ -0,0 +1,27 @@
+package com.example.rent2gojavaproject.services.concretes;
+
+import com.cloudinary.Cloudinary;
+import com.example.rent2gojavaproject.services.abstracts.FileUpload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class FileUploadManager implements FileUpload {
+
+    private final Cloudinary cloudinary;
+
+    @Override
+    public String uploadFile(MultipartFile multipartFile) throws IOException {
+        return cloudinary.uploader()
+                .upload(multipartFile.getBytes(),
+                        Map.of("public_id", UUID.randomUUID().toString()))
+                .get("url")
+                .toString();
+    }
+}
