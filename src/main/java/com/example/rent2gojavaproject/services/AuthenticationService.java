4ac95dfe964978978e8a83c9aaf12458124f0551
ShmsErl <erol.sehmus1@gmail.com>
2024-01-20 17:46:28 +0300

    Email verification structure has been integrated into jwt token and service structure.

--- src/main/java/com/example/rent2gojavaproject/services/AuthenticationService.java
+++ src/main/java/com/example/rent2gojavaproject/services/AuthenticationService.java
@@ -3,7 +3,12 @@ package com.example.rent2gojavaproject.services;
 import com.example.rent2gojavaproject.core.exceptions.NotFoundException;
 import com.example.rent2gojavaproject.models.Role;
 import com.example.rent2gojavaproject.models.User;
+import com.example.rent2gojavaproject.registration.EmailValidator;
+import com.example.rent2gojavaproject.registration.RegistrationService;
+import com.example.rent2gojavaproject.registration.token.ConfirmationToken;
+import com.example.rent2gojavaproject.registration.token.ConfirmationTokenService;
 import com.example.rent2gojavaproject.repositories.UserRepository;
+import com.example.rent2gojavaproject.services.abstracts.EmailSenderService;
 import com.example.rent2gojavaproject.services.abstracts.UserService;
 import com.example.rent2gojavaproject.services.dtos.requests.userRequest.SignInRequest;
 import com.example.rent2gojavaproject.services.dtos.requests.userRequest.SignUpRequest;
@@ -11,20 +16,35 @@ import com.example.rent2gojavaproject.services.dtos.responses.userResponse.JwtAu
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
-public class AuthenticationService {
+public class AuthenticationService implements  AuthenticationManager {
 
     private final UserRepository userRepository;
     private final UserService userService;
+    private final EmailValidator emailValidator;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
+    private final EmailSenderService emailSender;
+    private final RegistrationService registrationService;
+    private final ConfirmationTokenService confirmationTokenService;
 
-    public JwtAuthenticationResponse signup(SignUpRequest request) {
+
+    public String signup(SignUpRequest request) {
+        boolean isValidEmail = emailValidator.
+                test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
         var user = User
                 .builder()
                 .name(request.getFirstName())
@@ -35,22 +55,59 @@ public class AuthenticationService {
                 .role(Role.USER)
                 .build();
 
-        user = userService.addUser(user);
-        var jwt = jwtService.generateToken(user);
 
-        return JwtAuthenticationResponse.builder().token(jwt).build();
+
+        String token = userService.addUser(user);
+        String link = "http://localhost:8080/api/confirm?token=" + token;
+        emailSender.send(
+                request.getEmail(),
+                registrationService.buildEmail(request.getFirstName(), link));
+
+
+        return token;
     }
 
 
-    public JwtAuthenticationResponse signin(SignInRequest request) {
+    public JwtAuthenticationResponse signin(SignInRequest request) throws Exception {
         var user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new NotFoundException("Invalid email name"));
+        if(!user.isEnabled()){
+            throw new Exception("User not enabled");
+        }
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))
             throw new NotFoundException("Invalid password ");
-        authenticationManager.authenticate(
+        authenticate(
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
 
         var jwt = jwtService.generateToken(user);
         return JwtAuthenticationResponse.builder().token(jwt).build();
+    }
+    @Transactional
+    public String confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService
+                .getToken(token)
+                .orElseThrow(() ->
+                        new IllegalStateException("token not found"));
+
+        if (confirmationToken.getConfirmedAt() != null) {
+            throw new IllegalStateException("email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if (expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+        userService.enableAppUser(
+                confirmationToken.getUser().getEmail());
+        return "confirmed";
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication)
+            throws AuthenticationException {
+        return authentication;
     }
 }
