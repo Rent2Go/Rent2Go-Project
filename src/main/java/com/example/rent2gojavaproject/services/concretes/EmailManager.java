94efba69fcd6bb897380c08ac3915f056720a2a1
ShmsErl <erol.sehmus1@gmail.com>
2024-01-22 00:00:25 +0300

    Codes Organised.

--- src/main/java/com/example/rent2gojavaproject/services/concretes/EmailManager.java
+++ src/main/java/com/example/rent2gojavaproject/services/concretes/EmailManager.java
@@ -52,19 +52,7 @@ public class EmailManager implements EmailSenderService {
         } catch (IOException ex) {
            throw new RuntimeException(ex.getMessage());
         }
-       /* try {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper =
-                    new MimeMessageHelper(mimeMessage, "utf-8");
-            helper.setText(email, true);
-            helper.setTo(to);
-            helper.setSubject("Confirm your email");
-            helper.setFrom("erol.sehmus1@gmail.com");
-            mailSender.send(mimeMessage);
-        } catch (MessagingException e) {
 
-            throw new IllegalStateException("failed to send email");
-        }*/
     }
 
 
