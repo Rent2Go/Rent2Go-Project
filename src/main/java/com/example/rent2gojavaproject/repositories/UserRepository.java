934cbcb8169709d4d995b7e3afc6493faf09748d
ShmsErl <erol.sehmus1@gmail.com>
2024-01-22 00:02:26 +0300

    Added two Arrived Method and updated password method.

--- src/main/java/com/example/rent2gojavaproject/repositories/UserRepository.java
+++ src/main/java/com/example/rent2gojavaproject/repositories/UserRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndName(String email, String name);
 
     boolean existsByEmail(String email);
 
@@ -20,5 +21,11 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("UPDATE User a " +
             "SET a.isEnabled = TRUE WHERE a.email = :email")
     int enableAppUser(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User a " +
+            "SET a.password = :password WHERE a.email = :email")
+    void passwordChange(String email,String password);
 
 }
