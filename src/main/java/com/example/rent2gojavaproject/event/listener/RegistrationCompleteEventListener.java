1fc737471f4ee2219985623223813a0e22ff0fae
Soner <syln.soner@gmail.com>
2024-01-23 01:32:25 +0300

    added password reset method in registration complete listener

--- src/main/java/com/example/rent2gojavaproject/event/listener/RegistrationCompleteEventListener.java
+++ src/main/java/com/example/rent2gojavaproject/event/listener/RegistrationCompleteEventListener.java
@@ -86,4 +86,46 @@ public class RegistrationCompleteEventListener
 
         mailSender.send(message);
     }
+
+    public void sendResetPasswordEmail(String url) throws MessagingException, UnsupportedEncodingException {
+        String subject = "Reset Password";
+        String senderName = "Rent2Go Company";
+
+        String htmlContent =
+                "<html>" +
+                        "<body style='font-family: \"Roboto\", sans-serif; margin:0; padding:0;'>" +
+                        "<div style='padding: 50px;'>" +
+                        "<div style='max-width: 600px; margin: auto; background-color: white; border-radius: 5px; padding: 20px; box-shadow: 0px 0px 10px rgba(0,0,0,0.1);'>" +
+                        "<h1 style='color: #445566; text-align:center;'>Reset Password</h1>" +
+                        "<p>Dear " + theUser.getName() + " " + theUser.getSurname() + ",</p>" +
+                        "<p>You have requested to reset your password. To complete your password reset, please click the button below:</p>" +
+                        "<div style='text-align:center; margin-bottom:20px;'>" +
+                        "<a href=\"" + url + "\" style='background-color: #5D9CEC; color: white; text-decoration: none; padding: 10px 20px; border-radius:5px; box-shadow: 0px 3px 6px rgba(0,0,0,0.1); display: inline-block;'>Reset Password</a>" +
+                        "</div>" +
+                        "<p>If the button doesn't work, you can also copy and paste the following link into your web browser:</p>" +
+                        "<p><a href=\"" + url + "\" style='color:#5D9CEC'>" + url + "</a></p>" +
+                        "<p>If you did not request a password reset, please ignore this email.</p>" +
+                        "<p style='border-top: 1px solid #DDDDDD; padding-top:20px; color:#888888'>Best Regards,<br>The Rent2Go Team</p>" +
+                        "</div>" +
+                        "</div>" +
+                        "</body>" +
+                        "</html>";
+
+        String textContent =
+                "Dear " + theUser.getName() + " " + theUser.getSurname() + ",\n" +
+                        "You have requested to reset your password. To complete your password reset, please visit the following URL:\n" +
+                        url + "\n" +
+                        "If you did not request a password reset, please ignore this email.\n" +
+                        "Best Regards,\nThe Rent2Go Team";
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+
+        messageHelper.setFrom("noreply@rentogo.com.tr", senderName);
+        messageHelper.setTo(theUser.getEmail());
+        messageHelper.setSubject(subject);
+        messageHelper.setText(textContent, htmlContent);
+
+        mailSender.send(message);
+    }
 }
