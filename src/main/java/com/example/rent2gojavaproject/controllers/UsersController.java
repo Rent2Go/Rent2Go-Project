91f32f61e6f89b686d4a076e4fca2954d8fc8819
Soner <syln.soner@gmail.com>
2024-01-24 14:58:33 +0300

    Revert "added CrosOrigin annotation for cors bug"
    This reverts commit 10c18847223dd67563c52c93b5fb69805f026fc1.

--- src/main/java/com/example/rent2gojavaproject/controllers/UsersController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/UsersController.java
@@ -19,7 +19,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
-@CrossOrigin
 public class UsersController {
     private final UserService userService;
 
