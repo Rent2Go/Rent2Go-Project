a87d18a5b2ce0132aac1781ab8c7b1da32c224f8
Soner <syln.soner@gmail.com>
2024-01-24 18:55:43 +0300

    Add ConfirmationTokenManager.java for managing confirmation tokens

--- src/main/java/com/example/rent2gojavaproject/core/registration/token/ConfirmationTokenManager.java
+++ src/main/java/com/example/rent2gojavaproject/core/registration/token/ConfirmationTokenManager.java
@@ -1,6 +1,9 @@
 package com.example.rent2gojavaproject.core.registration.token;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -9,6 +12,7 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class ConfirmationTokenManager implements ConfirmationTokenService {
+    private static final Logger logger = LoggerFactory.getLogger(ConfirmationTokenService.class);
 
     private final ConfirmationTokenRepository confirmationTokenRepository;
 
@@ -17,11 +21,18 @@ public class ConfirmationTokenManager implements ConfirmationTokenService {
     }
 
     public Optional<ConfirmationToken> getToken(String token) {
+
         return confirmationTokenRepository.findByToken(token);
+
     }
 
     public int setConfirmedAt(String token) {
         return confirmationTokenRepository.updateConfirmedAt(
                 token, LocalDateTime.now());
+    }
+
+    @Override
+    public void deleteConfirmationToken(String token) {
+        confirmationTokenRepository.deleteConfirmationToken(token);
     }
 }
