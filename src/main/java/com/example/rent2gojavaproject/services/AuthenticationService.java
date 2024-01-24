1e07790b254574c2a77374dfa741f479e934cc28
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-24 13:48:53 +0300

    Update AuthenticationService.java
    Co-Authored-By: Soner <74818317+sonersyln@users.noreply.github.com>
    Co-Authored-By: Feyza Karanfil Erat <56940562+feyzaerat@users.noreply.github.com>

--- src/main/java/com/example/rent2gojavaproject/services/AuthenticationService.java
+++ src/main/java/com/example/rent2gojavaproject/services/AuthenticationService.java
@@ -21,5 +21,42 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
+    public User signup(SignUpRequest request) {
+        Optional<User> user = userRepository.findByEmail(request.getEmail());
+        var newUser = User
+                .builder()
+                .name(request.getFirstName())
+                .surname(request.getLastName())
+                .phoneNumber(request.getPhoneNumber())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .createdAt(LocalDate.now())
+                .build();
+
+        newUser = userService.addUser(newUser);
+        var jwt = jwtService.generateToken(newUser);
+
+        return newUser;
+    }
+
+
+
+    public JwtAuthenticationResponse signin(SignInRequest request) {
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new NotFoundException("Invalid email name"));
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))
+            throw new NotFoundException("Invalid password ");
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+        var jwt = jwtService.generateToken(user);
+        return JwtAuthenticationResponse.builder().token(jwt).build();
+    }
 }
