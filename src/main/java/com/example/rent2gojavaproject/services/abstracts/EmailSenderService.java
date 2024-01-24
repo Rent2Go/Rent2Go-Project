20466c0d44a262a535779ab44a03db6c6724f1f1
Soner <syln.soner@gmail.com>
2024-01-24 18:59:44 +0300

    update EmailSenderService

--- src/main/java/com/example/rent2gojavaproject/services/abstracts/EmailSenderService.java
+++ src/main/java/com/example/rent2gojavaproject/services/abstracts/EmailSenderService.java
@@ -1,15 +1,18 @@
 package com.example.rent2gojavaproject.services.abstracts;
 
+import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.UnsupportedEncodingException;
 
 public interface EmailSenderService {
 
 
-    void send(String to, String email);
+    void sendEmail(String name, String email, String link, String subject, String htmlContent, String textContent) throws MessagingException, UnsupportedEncodingException;
 
     public String applicationUrl(HttpServletRequest request);
 
-    String buildEmail(String name, String link) ;
+    void buildEmail(String name,String email, String link) throws MessagingException, UnsupportedEncodingException;
 
-    String sendResetPasswordEmail(String name,String url);
+    void sendResetPasswordEmail(String name,String email, String url) throws MessagingException, UnsupportedEncodingException;
 }
