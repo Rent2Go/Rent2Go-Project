8cb1ea38333a92b5f5afb89f0afc0ad35ff998eb
Soner <syln.soner@gmail.com>
2024-01-24 18:52:42 +0300

    Added new exceptions in AuthenticationController.java

--- src/main/java/com/example/rent2gojavaproject/controllers/AuthenticationController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/AuthenticationController.java
@@ -4,12 +4,13 @@ import com.example.rent2gojavaproject.services.concretes.AuthenticationService;
 import com.example.rent2gojavaproject.services.dtos.requests.userRequest.SignInRequest;
 import com.example.rent2gojavaproject.services.dtos.requests.userRequest.SignUpRequest;
 import com.example.rent2gojavaproject.services.dtos.responses.userResponse.JwtAuthenticationResponse;
+import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/api")
@@ -20,7 +21,7 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/signup")
-    public String signup(@Valid @RequestBody SignUpRequest request, HttpServletRequest servletRequest) {
+    public String signup(@Valid @RequestBody SignUpRequest request, HttpServletRequest servletRequest) throws MessagingException, UnsupportedEncodingException {
         return authenticationService.signup(request,servletRequest);
     }
 
