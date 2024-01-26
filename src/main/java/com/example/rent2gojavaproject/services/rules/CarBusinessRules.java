0adf22a98baaf995896b77a051512e461580e7ac
Soner <syln.soner@gmail.com>
2024-01-26 23:07:42 +0300

    Add imports and update exception messages

--- src/main/java/com/example/rent2gojavaproject/services/rules/CarBusinessRules.java
+++ src/main/java/com/example/rent2gojavaproject/services/rules/CarBusinessRules.java
@@ -2,9 +2,11 @@ package com.example.rent2gojavaproject.services.rules;
 
 import com.example.rent2gojavaproject.core.exceptions.AlreadyExistsException;
 import com.example.rent2gojavaproject.core.exceptions.NotFoundException;
+import com.example.rent2gojavaproject.core.utilities.constants.MessageConstants;
 import com.example.rent2gojavaproject.repositories.CarRepository;
 import com.example.rent2gojavaproject.services.abstracts.ColorService;
 import com.example.rent2gojavaproject.services.abstracts.ModelService;
+import jakarta.mail.Message;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,15 +23,20 @@ public class CarBusinessRules {
         String licensePlate = plate.replace(" ", "").toUpperCase();
         boolean result = this.carRepository.existsByPlate(licensePlate);
         if (result) {
-            throw new AlreadyExistsException("Car Plate already exists! : " + plate);
+            throw new AlreadyExistsException(plate + MessageConstants.ALREADY_EXISTS.getMessage());
         }
 
         return licensePlate;
     }
 
     public void updateCarMethod(int modelId, int colorId) {
-        if (!(modelService.existsById(modelId) && colorService.existsById(colorId))) {
-            throw new NotFoundException("Model or color does not exist");
+
+        if (!modelService.existsById(modelId)) {
+            throw new NotFoundException(MessageConstants.MODEL.getMessage() + MessageConstants.NOT_FOUND.getMessage());
         }
+        if (!colorService.existsById(colorId)) {
+            throw new NotFoundException(MessageConstants.COLOR.getMessage() + MessageConstants.NOT_FOUND.getMessage());
+        }
+
     }
 }
