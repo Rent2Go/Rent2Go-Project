ca405b6bda856b985e55abe9e26b21d03f48c9e6
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-06 18:06:54 +0300

    created service for page settings

--- src/main/java/com/example/rent2gojavaproject/services/abstracts/PageSettingsService.java
+++ src/main/java/com/example/rent2gojavaproject/services/abstracts/PageSettingsService.java
@@ -1,2 +1,27 @@
-package com.example.rent2gojavaproject.services.abstracts;public interface PageSettingsService {
+package com.example.rent2gojavaproject.services.abstracts;
+
+import com.example.rent2gojavaproject.core.utilities.results.DataResult;
+import com.example.rent2gojavaproject.core.utilities.results.Result;
+import com.example.rent2gojavaproject.services.dtos.requests.pageSettingsRequest.AddSettingRequest;
+import com.example.rent2gojavaproject.services.dtos.requests.pageSettingsRequest.UpdateSettingRequest;
+import com.example.rent2gojavaproject.services.dtos.responses.pageSettingsResponse.GetPageSettingListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.pageSettingsResponse.GetPageSettingResponse;
+import io.jsonwebtoken.io.IOException;
+
+
+import java.util.List;
+
+public interface PageSettingsService {
+
+    DataResult<List<GetPageSettingListResponse>>getAllSettings();
+
+    DataResult<GetPageSettingResponse> getById(int id);
+
+    Result addSetting(AddSettingRequest addSettingRequest) throws IOException;
+
+    Result updateSetting(UpdateSettingRequest updateSettingRequest);
+
+
+
+
 }
