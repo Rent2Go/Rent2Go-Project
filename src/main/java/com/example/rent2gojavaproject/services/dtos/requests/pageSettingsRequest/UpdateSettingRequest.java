feb6c3e72ed9ddd51fd74c4ce5e549da77d89952
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-06 18:04:00 +0300

    created requests for page settings

---
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/requests/pageSettingsRequest/UpdateSettingRequest.java
@@ -0,0 +1,11 @@
+package com.example.rent2gojavaproject.services.dtos.requests.pageSettingsRequest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateSettingRequest {
+}
