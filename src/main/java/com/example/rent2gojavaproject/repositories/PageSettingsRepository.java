414edf0f1169a9c63846e0be63bbf0aa8f8f4df0
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-06 17:50:52 +0300

    created PageSettingsRepository

---
+++ src/main/java/com/example/rent2gojavaproject/repositories/PageSettingsRepository.java
@@ -0,0 +1,9 @@
+package com.example.rent2gojavaproject.repositories;
+
+import com.example.rent2gojavaproject.models.PageSettings;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PageSettingsRepository extends JpaRepository<PageSettings, Integer> {
+
+
+}
