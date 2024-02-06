2566de72628ee7c749b55f4682049a797f45de8c
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-06 18:00:44 +0300

    created responses for page settings

---
+++ src/main/java/com/example/rent2gojavaproject/services/abstracts/PageSettingsService.java
@@ -0,0 +1,2 @@
+package com.example.rent2gojavaproject.services.abstracts;public interface PageSettingsService {
+}
---
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/pageSettingsResponse/GetPageSettingListResponse.java
@@ -0,0 +1,22 @@
+package com.example.rent2gojavaproject.services.dtos.responses.pageSettingsResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class GetPageSettingListResponse {
+    private int id;
+    private String title;
+    private String url;
+    private String logo;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String linkedin;
+    private String instagram;
+    private String github;
+}
