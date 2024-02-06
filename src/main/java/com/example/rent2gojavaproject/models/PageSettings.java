3b3ad5b29dd10e3e6728ad116e7c5882c41beeae
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-06 17:48:17 +0300

    created page settings entity for settings table

---
+++ src/main/java/com/example/rent2gojavaproject/models/PageSettings.java
@@ -0,0 +1,45 @@
+package com.example.rent2gojavaproject.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="settings")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageSettings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title", nullable = false)
+    private String name;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Column(name = "logo", nullable = false)
+    private String logo;
+
+    @Column(name = "phoneNumber", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "linkedin", nullable = false)
+    private String linkedin;
+
+    @Column(name = "instagram", nullable = false)
+    private String instagram;
+
+    @Column(name = "github", nullable = false)
+    private String github;
+}
