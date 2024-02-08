41d70ad69c100522c88799704afa5e7e5cb48194
ShmsErl <erol.sehmus1@gmail.com>
2024-02-08 13:30:31 +0300

    imageUpdate and isActiveUpdate endpoint  are defined.

--- src/main/java/com/example/rent2gojavaproject/controllers/CarsController.java
+++ src/main/java/com/example/rent2gojavaproject/controllers/CarsController.java
@@ -1,16 +1,13 @@
 package com.example.rent2gojavaproject.controllers;
 
 import com.example.rent2gojavaproject.core.utilities.results.DataResult;
-import com.example.rent2gojavaproject.core.utilities.results.ErrorsResult;
+
 import com.example.rent2gojavaproject.core.utilities.results.Result;
 import com.example.rent2gojavaproject.services.abstracts.CarService;
 import com.example.rent2gojavaproject.services.dtos.requests.carRequest.AddCarRequest;
 import com.example.rent2gojavaproject.services.dtos.requests.carRequest.UpdateCarRequest;
 import com.example.rent2gojavaproject.services.dtos.responses.carResponse.GetCarListResponse;
 import com.example.rent2gojavaproject.services.dtos.responses.carResponse.GetCarResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -40,24 +37,29 @@ public class CarsController {
         return this.carService.getById(id);
     }
 
-    @PostMapping("/" )
+    @PostMapping( )
     @ResponseStatus(code = HttpStatus.CREATED)
     public Result createCar(@RequestPart("addCarRequest") AddCarRequest addCarRequest, @RequestPart("file") MultipartFile file) throws IOException {
 
         return this.carService.addCar(addCarRequest,file);
     }
-    @PatchMapping("/imageupdate")
+    @PostMapping("/imageupdate")
     public Result updateCarImage(@RequestParam("plate") String carPlate , @RequestParam("file") MultipartFile file) throws IOException {
 
         return  this.carService.updateCarImage(carPlate,file);
     }
 
 
-    @PutMapping("/")
+    @PutMapping()
     @ResponseStatus(code = HttpStatus.OK)
     public Result updateCar(@RequestBody @Valid UpdateCarRequest updateCarRequest) {
 
         return this.carService.updateCar(updateCarRequest);
+    }
+    @PatchMapping("/isactive/{id}")
+    public Result updateCarIsActive(@PathVariable("id") int id,  @RequestParam boolean isActive) {
+
+        return this.carService.updateCarIsActive(id,isActive);
     }
 
     @DeleteMapping("/{id}")
