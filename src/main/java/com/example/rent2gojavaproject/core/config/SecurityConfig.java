5736d07d25ff87583f7fef3e0f4f37fe230dcb9f
ShmsErl <erol.sehmus1@gmail.com>
2024-02-08 13:32:05 +0300

    Apis paths have been added to the security config file.

--- src/main/java/com/example/rent2gojavaproject/core/config/SecurityConfig.java
+++ src/main/java/com/example/rent2gojavaproject/core/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
             "/swagger-ui/**",
             "/webjars/**",
             "/swagger-ui.html",
+            "/**"
             };
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserManager userService;
@@ -63,7 +64,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 //proje deploy etmeden önce cors u sil
-                .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()))
+
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(WHITE_LIST_URL).permitAll()
@@ -76,10 +77,11 @@ public class SecurityConfig {
                                 "api/users/resetpassword",
                                 "api/admins/signin",
                                 "api/refreshtoken").permitAll()
+                        .requestMatchers(HttpMethod.PATCH, "api/cars/isactive/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/confirm/**", "api/cars", "api/colors", "api/brands", "api/models", "api/cars/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/test/users").hasAuthority("ROLE_USER")
                         .requestMatchers(HttpMethod.POST, "api/users/changePassword").hasAuthority("ROLE_USER")
-                        .requestMatchers(HttpMethod.POST, "api/cars/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.POST, "api/cars").permitAll()
                        .anyRequest().authenticated()
 
                 )
