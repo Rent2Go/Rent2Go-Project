d465d9aa7eed9520d325383055a8c2fb74b0d34d
ShmsErl <erol.sehmus1@gmail.com>
2024-02-11 11:54:20 +0300

    Created PageSettingsController classç

---
+++ src/main/java/com/example/rent2gojavaproject/controllers/PageSettingsController.java
@@ -0,0 +1,50 @@
+package com.example.rent2gojavaproject.controllers;
+
+import com.example.rent2gojavaproject.core.utilities.results.DataResult;
+import com.example.rent2gojavaproject.core.utilities.results.Result;
+import com.example.rent2gojavaproject.services.abstracts.PageSettingsService;
+import com.example.rent2gojavaproject.services.dtos.requests.pageSettingsRequest.AddSettingRequest;
+import com.example.rent2gojavaproject.services.dtos.requests.pageSettingsRequest.UpdateSettingRequest;
+import com.example.rent2gojavaproject.services.dtos.responses.pageSettingsResponse.GetPageSettingListResponse;
+import com.example.rent2gojavaproject.services.dtos.responses.pageSettingsResponse.GetPageSettingResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/settings")
+@AllArgsConstructor
+@CrossOrigin
+public class PageSettingsController {
+
+    private final PageSettingsService settingsService;
+
+
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public DataResult<List<GetPageSettingListResponse>> getAllSettings(){
+
+        return this.settingsService.getAllSettings();
+    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public DataResult<GetPageSettingResponse> getById(int id){
+
+        return this.settingsService.getById(id);
+    }
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Result addSetting(AddSettingRequest addSettingRequest){
+
+            return this.settingsService.addSetting(addSettingRequest);
+    }
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public Result updateSetting(UpdateSettingRequest updateSettingRequest){
+
+        return this.settingsService.updateSetting(updateSettingRequest);
+    }
+}
