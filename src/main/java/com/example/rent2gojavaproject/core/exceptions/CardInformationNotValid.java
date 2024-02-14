b1e30b3bd3280f45692be58c6d01fa359bc298f2
ShmsErl <erol.sehmus1@gmail.com>
2024-02-14 22:08:32 +0300

    Created CardInformationNotValid exception method.

---
+++ src/main/java/com/example/rent2gojavaproject/core/exceptions/CardInformationNotValid.java
@@ -0,0 +1,7 @@
+package com.example.rent2gojavaproject.core.exceptions;
+
+public class CardInformationNotValid extends RuntimeException{
+    public CardInformationNotValid(String message) {
+        super(message);
+    }
+}
