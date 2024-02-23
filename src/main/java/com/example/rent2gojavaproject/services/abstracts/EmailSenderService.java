262f77cd59526d2a59e4d9eaf30e46c85561c29e
Soner <syln.soner@gmail.com>
2024-02-24 02:56:32 +0300

    add new methods to emailsenderservice

--- src/main/java/com/example/rent2gojavaproject/services/abstracts/EmailSenderService.java
+++ src/main/java/com/example/rent2gojavaproject/services/abstracts/EmailSenderService.java
@@ -1,12 +1,17 @@
 package com.example.rent2gojavaproject.services.abstracts;
 
+import com.example.rent2gojavaproject.services.dtos.requests.contactFormRequest.ContactFormRequest;
+import com.example.rent2gojavaproject.services.dtos.requests.reservationDetailRequest.AddReservationDetailRequest;
 import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import java.io.UnsupportedEncodingException;
 
 public interface EmailSenderService {
 
+
+    JavaMailSender getJavaMailSender();
 
     void sendEmail(String name, String email, String link, String subject, String htmlContent, String textContent) throws MessagingException, UnsupportedEncodingException;
 
@@ -15,4 +20,10 @@ public interface EmailSenderService {
     void buildEmail(String name,String email, String link) throws MessagingException, UnsupportedEncodingException;
 
     void sendResetPasswordEmail(String name,String email, String url) throws MessagingException, UnsupportedEncodingException;
+
+    void sendContactEmail(ContactFormRequest form) throws MessagingException, UnsupportedEncodingException;
+
+    void sendThankYouEmail(ContactFormRequest form) throws MessagingException, UnsupportedEncodingException;
+
+    void reservationDetailEmail(AddReservationDetailRequest form) throws MessagingException, UnsupportedEncodingException;
 }
