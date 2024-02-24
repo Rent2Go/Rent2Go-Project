f30aa40d267325d88a2a5b39210dd482ca6c0d59
ShmsErl <erol.sehmus1@gmail.com>
2024-02-24 16:08:25 +0300

    The response structure of the Customer object has been updated.

--- src/main/java/com/example/rent2gojavaproject/services/dtos/responses/customerResponse/GetCustomerResponse.java
+++ src/main/java/com/example/rent2gojavaproject/services/dtos/responses/customerResponse/GetCustomerResponse.java
@@ -12,6 +12,15 @@ import lombok.NoArgsConstructor;
 public class GetCustomerResponse {
 
     private int id;
-    private User user;
+    private int userId;
+    private String userImageUrl;
+    private String userName;
+    private String userSurname;
+    private String userEmail;
+    private String userPhoneNumber;
+    private String userIdCardNumber;
+    private String userCityName;
+    private String userDistrictName;
+    private String userAddress;
 
 }
